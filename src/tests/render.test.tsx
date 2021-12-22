79fa4d21ae68c5fe820a031800f0aedadcf5639b
JaFe <kontakt.jakubfedoszczak@gmail.com>
2021-12-22 08:52:02 +0100

    Fixup! Add example test

--- src/tests/render.test.tsx
+++ src/tests/render.test.tsx
@@ -3,5 +3,5 @@ import App from 'views/App';
 
 it('Renders application', () => {
   render(<App />);
-  expect(screen.getByText('Hello World!')).toBeInTheDocument();
+  expect(screen.getByText('Hello World')).toBeInTheDocument();
 });
