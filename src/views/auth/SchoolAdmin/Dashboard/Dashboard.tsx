295613bc2e9c31c2ada4b673d22af9dc777cc265
JaFe <kontakt.jakubfedoszczak@gmail.com>
2022-01-05 13:59:06 +0100

    Remove useless template declarations

--- src/views/auth/SchoolAdmin/Dashboard/Dashboard.tsx
+++ src/views/auth/SchoolAdmin/Dashboard/Dashboard.tsx
@@ -1,9 +1,9 @@
-import SchoolAdminTemplate from 'components/templates/SchoolAdminTemplate/SchoolAdminTemplate';
 import { Grid, Heading, Wrapper } from './Dashboard.styles';
 import ClassIcon from 'assets/icons/ClassIcon.png';
 import StudentIcon from 'assets/icons/StudentIcon.png';
 import InfoCard from 'components/molecules/InfoCard/InfoCard';
 import ClassesCard from 'components/organisms/ClassesCard/ClassesCard';
+import React from 'react';
 
 const mockData = {
   name1: 'Łączna liczba użytkowników',
@@ -14,16 +14,14 @@ const mockData = {
 };
 
 const Dashboard: React.FC = () => (
-  <SchoolAdminTemplate>
-    <Wrapper>
-      <Heading>Witaj {mockData.admin}!</Heading>
-      <Grid>
-        <InfoCard name={mockData.name1} number={mockData.numberOfAccounts} icon={StudentIcon} />
-        <InfoCard name={mockData.name2} number={mockData.numberOfClasses} icon={ClassIcon} />
-        <ClassesCard />
-      </Grid>
-    </Wrapper>
-  </SchoolAdminTemplate>
+  <Wrapper>
+    <Heading>Witaj {mockData.admin}!</Heading>
+    <Grid>
+      <InfoCard name={mockData.name1} number={mockData.numberOfAccounts} icon={StudentIcon} />
+      <InfoCard name={mockData.name2} number={mockData.numberOfClasses} icon={ClassIcon} />
+      <ClassesCard />
+    </Grid>
+  </Wrapper>
 );
 
 export default Dashboard;
