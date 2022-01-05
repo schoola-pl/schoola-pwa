df20fa59a425a026bf1ee20806ed2ee01a21ded6
JaFe <kontakt.jakubfedoszczak@gmail.com>
2022-01-05 14:04:11 +0100

    Modify routes control file

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import React from 'react';
 import Home from 'views/Home';
 import Login from 'views/auth/Forms/Login/Login';
+import SchoolAdminTemplate from '../components/templates/SchoolAdminTemplate/SchoolAdminTemplate';
 
 interface routesInterface {
   // Path to component
@@ -11,25 +12,32 @@ interface routesInterface {
   isProtected: boolean;
   // Who can access this route?
   role: string;
-  // Where to redirect if user is not authenticated?
-  redirectTo?: string;
 }
+
+// Paths
+export const paths = {
+  login: '/login',
+  student: '/student/*',
+  schoolAdmin: '/school-admin/*'
+};
 
 // Roles
 export const roles: { [key: string]: string } = {
   public: 'none',
   authenticated: 'any',
-  student: 'Student'
+  student: 'Student',
+  schoolAdmin: 'School Admin'
 };
 
 // Environment routes
-const dashboardRoute = '/';
-const loginRoute = '/login';
+const dashboardRoute = paths.student;
+const loginRoute = paths.login;
 
 // Array with routes in application;
 const routes: routesInterface[] = [
-  { path: dashboardRoute, Component: Home, isProtected: true, role: roles.authenticated },
-  { path: loginRoute, Component: Login, isProtected: false, role: roles.public }
+  { path: loginRoute, Component: Login, isProtected: false, role: roles.public },
+  { path: paths.student, Component: Home, isProtected: true, role: roles.student },
+  { path: paths.schoolAdmin, Component: SchoolAdminTemplate, isProtected: true, role: roles.schoolAdmin }
 ];
 
 // Exports
