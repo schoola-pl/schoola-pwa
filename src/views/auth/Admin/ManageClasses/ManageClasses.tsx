b512f635929b31c11f1c79d3abab8e70436f47dc
TeodorWolski <teodorwolski88@gmail.com>
2022-01-06 16:04:49 +0100

    Static Version

--- src/views/auth/Admin/ManageClasses/ManageClasses.tsx
+++ src/views/auth/Admin/ManageClasses/ManageClasses.tsx
@@ -14,10 +14,16 @@ const Links = styled.div`
   align-items: center;
   padding-top: 1.5rem;
   border-bottom: 3px solid #eceff7;
+  transition: all 0.1s linear;
+
+  &:hover {
+    border-bottom: 3px solid ${({ theme }) => theme.colors.accentGreen};
+  }
 `;
 
 const ContentWrapper = styled.div`
   width: 100%;
+  margin-bottom: 2rem;
 `;
 
 const mockClasses = [
@@ -33,7 +39,8 @@ const mockClasses = [
 
 const Wrapper = styled.div`
   padding: 1rem;
-  overflow-x: none;
+  overflow-x: hidden;
+  overflow-y: scroll;
   display: flex;
   flex-direction: column;
 `;
@@ -49,20 +56,8 @@ const HeadingLink = styled.h1`
   transition: all 0.1s linear;
   cursor: pointer;
 
-  //   &:after {
-  //     content: '';
-  //     top: 0.75rem;
-  //     transition: all 0.1s linear;
-  //     position: absolute;
-  //     padding-top: 0.5rem;
-  //     border-bottom: 3px solid #eceff7;
-  //   }
-
   &:hover {
-    // &:after {
-    //   content: '';
-    //   border-bottom: 3px solid ${({ theme }) => theme.colors.accentGreen};
-    // }
+    color: ${({ theme }) => theme.colors.lightGreen};
   }
 `;
 
@@ -82,6 +77,73 @@ const AmountWrapper = styled.div`
   }
 `;
 
+const ClassesWrapper = styled.div`
+  padding-left: 3rem;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+`;
+
+const ClassDetails = styled.details`
+  height: 5.5rem;
+  width: 35rem;
+  background-color: white;
+  display: flex;
+  max-width: 35rem;
+  border-radius: 1rem;
+  border-top: 5px solid ${({ theme }) => theme.colors.accentGreen};
+  text-decoration: none;
+  position: relative;
+  box-shadow: -2px 4px 10px rgba(115, 124, 142, 0.09);
+
+  summary {
+    list-style: none;
+    padding: 1rem;
+    font-size: ${({ theme }) => theme.fontSize.s};
+    font-weight: ${({ theme }) => theme.fontWeight.semibold};
+  }
+
+  summary::after {
+    content: '>';
+    position: absolute;
+    // left: -10px;
+    right: 20px;
+    transition: transform 0.1s ease-in-out;
+    transform: rotate(90deg);
+  }
+
+  &[open] {
+    summary::after {
+      transform: rotate(-90deg);
+    }
+  }
+`;
+
+const ClassWrapper = styled.div`
+  background-color: white;
+  margin-top: 3rem;
+  padding: 1rem;
+  border-radius: 1rem;
+  box-shadow: -2px 4px 10px rgba(115, 124, 142, 0.09);
+
+  p {
+    font-size: ${({ theme }) => theme.fontSize.s};
+
+    strong {
+      color: ${({ theme }) => theme.colors.accentBlue};
+    }
+  }
+`;
+
+const ClassCircle = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 5rem;
+  height: 5rem;
+  width: 5rem;
+  background-color: #eceff7;
+`;
+
 const ManageClasses: React.FC = () => (
   <AdminTemplate>
     <Wrapper>
@@ -97,6 +159,61 @@ const ManageClasses: React.FC = () => (
             Łączna ilosć klas: <span>16</span>
           </h1>
         </AmountWrapper>
+        <ClassesWrapper>
+          <div>
+            <ClassDetails>
+              <summary>Klasy Pierwsze</summary>
+              <ClassWrapper>
+                <ClassCircle>
+                  <h1>1A</h1>
+                </ClassCircle>
+                <p>liczba uczniów: 30</p>
+              </ClassWrapper>
+              <ClassWrapper>
+                <h1>1B</h1>
+                <p>liczba uczniów: 30</p>
+              </ClassWrapper>
+              <ClassWrapper>
+                <h1>1C</h1>
+                <p>liczba uczniów: 30</p>
+              </ClassWrapper>
+            </ClassDetails>
+          </div>
+          <div>
+            <ClassDetails>
+              <summary>Klasy drugie</summary>
+              <ClassWrapper>
+                <h1>2A</h1>
+                <p>liczba uczniów: 30</p>
+              </ClassWrapper>
+              <ClassWrapper>
+                <h1>2B</h1>
+                <p>liczba uczniów: 30</p>
+              </ClassWrapper>
+              <ClassWrapper>
+                <h1>2C</h1>
+                <p>liczba uczniów: 30</p>
+              </ClassWrapper>
+            </ClassDetails>
+          </div>
+          <div>
+            <ClassDetails>
+              <summary>Klasy trzecie</summary>
+              <ClassWrapper>
+                <h1>3A</h1>
+                <p>liczba uczniów: 30</p>
+              </ClassWrapper>
+              <ClassWrapper>
+                <h1>3B</h1>
+                <p>liczba uczniów: 30</p>
+              </ClassWrapper>
+              <ClassWrapper>
+                <h1>3C</h1>
+                <p>liczba uczniów: 30</p>
+              </ClassWrapper>
+            </ClassDetails>
+          </div>
+        </ClassesWrapper>
       </ContentWrapper>
     </Wrapper>
   </AdminTemplate>
