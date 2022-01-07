3e6b50d1e402c0f0bb875f411dd71cf332afabe4
JaFe <kontakt.jakubfedoszczak@gmail.com>
2022-01-07 12:34:05 +0100

    fixup! Add class RTK query

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,18 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit';
 import AuthAPI from './Api/Auth';
+import ClassAPI from './Api/Class';
 import userSlice from './Slices/User';
 
 const store = configureStore({
   reducer: {
     user: userSlice.reducer,
-    [AuthAPI.reducerPath]: AuthAPI.reducer
+    [AuthAPI.reducerPath]: AuthAPI.reducer,
+    [ClassAPI.reducerPath]: ClassAPI.reducer
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(AuthAPI.middleware)
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(AuthAPI.middleware).concat(ClassAPI.middleware)
 });
 
 const storeState = store.getState();
 export type storeRoot = typeof storeState;
 
 export * from './Api/Auth';
+export * from './Api/Class';
 export * from './Slices/User';
 export default store;
