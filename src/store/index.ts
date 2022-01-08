c29de75fadf5c2031376bce67f28a8ae7887aee8
JaFe <kontakt.jakubfedoszczak@gmail.com>
2022-01-08 08:20:20 +0100

    Connect user API to store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import AuthAPI from './Api/Auth';
 import ClassAPI from './Api/Class';
+import UserAPI from './Api/User';
 import userSlice from './Slices/User';
 
 const store = configureStore({
   reducer: {
     user: userSlice.reducer,
     [AuthAPI.reducerPath]: AuthAPI.reducer,
-    [ClassAPI.reducerPath]: ClassAPI.reducer
+    [ClassAPI.reducerPath]: ClassAPI.reducer,
+    [UserAPI.reducerPath]: UserAPI.reducer
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(AuthAPI.middleware).concat(ClassAPI.middleware)
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(AuthAPI.middleware).concat(ClassAPI.middleware).concat(UserAPI.middleware)
 });
 
 const storeState = store.getState();
@@ -17,5 +19,6 @@ export type storeRoot = typeof storeState;
 
 export * from './Api/Auth';
 export * from './Api/Class';
+export * from './Api/User';
 export * from './Slices/User';
 export default store;
