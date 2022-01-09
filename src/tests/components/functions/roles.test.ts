62f638cd4b82596904afc4ee52f13c3be9846a6b
JaFe <kontakt.jakubfedoszczak@gmail.com>
2022-01-09 17:59:44 +0100

    Remove useless comment

--- src/tests/components/functions/roles.test.ts
+++ src/tests/components/functions/roles.test.ts
@@ -4,7 +4,6 @@ describe('Functions > redirectPaths', () => {
   it('Checks if the redirectPaths function returns the correct path for a given role', () => {
     expect(getPathForRole('Student')).toBe('/student/');
     expect(getPathForRole('School Admin')).toBe('/school-admin/');
-    // Incorrect role
     expect(getPathForRole('Incorrect role')).toBe('/student/');
   });
   it('Checks if the redirectPaths function returns the correct path when input is incorrect', () => {
