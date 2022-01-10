74c76a3c20d0ac3e57d0039a0e913aed565ae235
TeodorWolski <teodorwolski88@gmail.com>
2022-01-10 20:14:51 +0100

    Class form

--- src/views/auth/Admin/AddClass/AddClass.tsx
+++ src/views/auth/Admin/AddClass/AddClass.tsx
@@ -1,10 +1,12 @@
 import AdminTemplate from 'components/templates/AdminTemplate/AdminTemplate';
 import styled from 'styled-components';
+import Input from 'components/atoms/Input/Input';
+import Button from 'components/atoms/Button/Button';
+import PeopleOptions from 'components/atoms/PeopleOptions/PeopleOptions';
 
 const Wrapper = styled.div`
   padding: 1rem;
   margin: 3rem 3rem 0;
-
   display: flex;
   flex-direction: column;
 `;
@@ -16,65 +18,69 @@ const Heading = styled.h1`
   padding-bottom: 1rem;
 `;
 
-// const Form =
+const Select = styled.select`
+  padding: 1.5rem 2rem;
+  box-sizing: border-box;
+  box-shadow: -2px 4px 10px rgba(115, 124, 142, 0.09);
+  border-radius: 5rem;
+  font-size: ${({ theme }) => theme.fontSize.s};
+  resize: none;
+  border: 2px solid ${({ theme }) => theme.colors.selectedItemGrey};
+  transition: border 0.3s linear;
+  margin: 2rem 0 2rem 0;
+  &:focus {
+    outline: none;
+    border: 2px solid ${({ theme }) => theme.colors.accentGreen};
+  }
+`;
+
+const StyledForm = styled.form`
+  margin-top: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  border-right: 2px solid #eceff7;
+  height: 100vh;
+  width: 30rem;
+`;
+
+const Label = styled.label`
+  font-size: ${({ theme }) => theme.fontSize.m};
+  margin-bottom: 2rem;
+`;
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+`;
+
+const PeopleSelect = styled(Select)`
+  transform: translateY(-20%);
+`;
 
 const AddClass = () => {
-  // const handleSubmit = (e.preventDefault) => {
-
-  // }
-
   return (
     <AdminTemplate>
       <Wrapper>
         <Heading>Dodaj klasę</Heading>
-        <form>
-          <label htmlFor="name">nazwa klasy</label>
-          <input type="text" name="name" />
-          <label htmlFor="amountOfStudents">Ilość osób w klasie</label>
-          <select name="amountOfStudents" id="">
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
-            <option value="10">10</option>
-            <option value="11">11</option>
-            <option value="12">12</option>
-            <option value="13">13</option>
-            <option value="14">14</option>
-            <option value="15">15</option>
-            <option value="16">16</option>
-            <option value="17">17</option>
-            <option value="18">18</option>
-            <option value="19">19</option>
-            <option value="20">20</option>
-            <option value="21">21</option>
-            <option value="22">22</option>
-            <option value="23">23</option>
-            <option value="24">24</option>
-            <option value="25">25</option>
-            <option value="26">26</option>
-            <option value="27">27</option>
-            <option value="28">28</option>
-            <option value="29">29</option>
-            <option value="30">30</option>
-            <option value="31">31</option>
-            <option value="32">32</option>
-            <option value="33">33</option>
-            <option value="34">34</option>
-            <option value="35">35</option>
-          </select>
-          <select name="" id="">
-            <option value="">Pierwsze klasy</option>
-            <option value="">Drugie klasy</option>
-            <option value="">Trzecie klasy</option>
-            <option value="">Czwarte klasy</option>
-          </select>
-        </form>
+        <Grid>
+          <StyledForm>
+            <Label htmlFor="name">Nazwa klasy</Label>
+            <Input type="text" name="name" placeholder="przykład: 1A" />
+            <Select name="" id="">
+              <option value="">Pierwsze klasy</option>
+              <option value="">Drugie klasy</option>
+              <option value="">Trzecie klasy</option>
+              <option value="">Czwarte klasy</option>
+            </Select>
+            <Label htmlFor="amountOfStudents">Ilość osób w klasie</Label>
+            <PeopleSelect name="amountOfStudents" id="">
+              <PeopleOptions />
+            </PeopleSelect>
+            <Button>Zatwierdź</Button>
+          </StyledForm>
+        </Grid>
       </Wrapper>
     </AdminTemplate>
   );
