fd40a3176c0119038e2f46c952a855adc02e383b
TeodorWolski <teodorwolski88@gmail.com>
2022-01-14 13:07:56 +0100

    ClassDetails view

---
+++ src/views/auth/Admin/ClassDetails/ClassDetails.styles.ts
@@ -0,0 +1,49 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  padding: 1rem;
+  margin: 1.5rem 3rem 0;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Heading = styled.h1`
+  font-size: 2.6rem;
+  font-weight: ${({ theme }) => theme.fontWeight.regular};
+  margin-bottom: 0.5rem;
+  span {
+    border-radius: 0.5rem;
+    margin-left: 0.5rem;
+    padding: 0.2rem 0.4rem;
+    color: white;
+    font-size: 2.2rem;
+    background-color: ${({ theme }) => theme.colors.accentBlue};
+  }
+`;
+
+export const InnerWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  margin-left: -5rem;
+`;
+
+export const InfoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  border-bottom: 3px solid #eceff7;
+  align-items: flex-start;
+`;
+
+export const Paragraph = styled.p`
+  font-size: ${({ theme }) => theme.fontSize.s};
+  margin-right: 15rem;
+`;
+
+export const ParagraphsWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  margin-left: 15rem;
+`;
--- src/views/auth/Admin/ClassDetails/ClassDetails.tsx
+++ src/views/auth/Admin/ClassDetails/ClassDetails.tsx
@@ -1,35 +1,95 @@
 import AdminTemplate from 'components/templates/AdminTemplate/AdminTemplate';
 import styled from 'styled-components';
+import EditIcon from 'assets/icons/EditIcon.png';
+import blueStudent from 'assets/icons/BlueStudent.svg';
+import SidebarLink from 'components/atoms/SidebarLink/SidebarLink';
+import DeleteIcon from 'assets/icons/DeleteIcon.svg';
+import { Wrapper, InfoWrapper, Heading, ParagraphsWrapper, Paragraph, InnerWrapper } from './ClassDetails.styles';
 
-export const Wrapper = styled.div`
-  padding: 1rem;
-  margin: 3rem 3rem 0;
+const PeopleCard = styled.div`
+  width: 95.8%;
+  height: 8.5rem;
+  background-color: white;
+  margin: 2rem 0rem 0 5rem;
+  border-radius: 1rem;
   display: flex;
-  flex-direction: column;
+  box-shadow: -2px 4px 10px rgba(115, 124, 142, 0.09);
+  align-items: center;
 `;
 
-export const Heading = styled.h1`
-  font-size: 2.6rem;
-  font-weight: ${({ theme }) => theme.fontWeight.regular};
-  border-bottom: 3px solid #eceff7;
-  padding-bottom: 1rem;
-
-  span {
-    border-radius: 0.5rem;
-    margin-left: 1rem;
-    padding: 0.25rem 0.4rem;
-    color: white;
-    font-size: 2.2rem;
-    background-color: ${({ theme }) => theme.colors.accentBlue};
-  }
+const StudentBox = styled(SidebarLink)`
+  background-color: #b8d0fc;
+  border-radius: 1rem;
+  height: 4.75rem;
+  width: 4.75rem;
+  padding: 1rem;
+  margin-left: 2rem;
 `;
 
+const DeleteBox = styled(SidebarLink)`
+  background-color: #fcb3b0;
+  border-radius: 1rem;
+  height: 4.75rem;
+  width: 4.75rem;
+  padding: 1rem;
+`;
+
+const EditBox = styled(SidebarLink)`
+  background-color: #e8fcd9;
+  border-radius: 1rem;
+  height: 4.75rem;
+  width: 4.75rem;
+  padding: 1rem;
+`;
+
+const Name = styled.p`
+  font-size: ${({ theme }) => theme.fontSize.s};
+  font-weight: ${({ theme }) => theme.fontWeight.medium};
+  margin-left: 7.5rem;
+  margin-right: 14rem;
+`;
+
+const Role = styled.p`
+  font-size: ${({ theme }) => theme.fontSize.s};
+  margin-right: 16.5rem;
+`;
+
+const Number = styled.p`
+  font-size: ${({ theme }) => theme.fontSize.s};
+  text-align: center;
+  margin-right: 10rem;
+`;
+
+const Date = styled.p`
+  font-size: ${({ theme }) => theme.fontSize.s};
+  text-align: center;
+  margin-right: 20rem;
+`;
 const ClassDetails = () => (
   <AdminTemplate>
     <Wrapper>
-      <Heading>
-        Edytuj <span>1A</span>
-      </Heading>
+      <InfoWrapper>
+        <Heading>
+          Edytuj <span>1A</span>
+        </Heading>
+        <ParagraphsWrapper>
+          <Paragraph>Imię i nazwisko</Paragraph>
+          <Paragraph>Rola</Paragraph>
+          <Paragraph>Data urodzenia</Paragraph>
+          <Paragraph>Numer</Paragraph>
+        </ParagraphsWrapper>
+      </InfoWrapper>
+      <InnerWrapper>
+        <PeopleCard>
+          <StudentBox icon={blueStudent} />
+          <Name>Marcin Najman</Name>
+          <Role>Uczeń</Role>
+          <Date>12.12.2012</Date>
+          <Number>1</Number>
+          <EditBox icon={EditIcon} />
+          <DeleteBox icon={DeleteIcon} />
+        </PeopleCard>
+      </InnerWrapper>
     </Wrapper>
   </AdminTemplate>
 );
