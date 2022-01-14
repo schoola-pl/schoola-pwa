dd96ae683ee7428309c4633e4b1a0f7f1c207bf2
JaFe <kontakt.jakubfedoszczak@gmail.com>
2022-01-14 05:07:29 +0100

    Add useUser hook with two methods
    Methods: logout, updateUserState

---
+++ src/hooks/useUser.tsx
@@ -0,0 +1,52 @@
+import React, { createContext, useContext } from 'react';
+import { authUser } from '../types/auth';
+import { removeJWT } from '../helpers/jwt';
+import { useNavigate } from 'react-router';
+import { useDispatch } from 'react-redux';
+import { addUser, removeUser } from '../store';
+
+interface UserContextTypes {
+  updateUserState: (user: authUser) => void;
+  logout: () => void;
+}
+
+const UserContext = createContext<UserContextTypes>({
+  updateUserState: () => {
+    throw new Error('UserContext is not initialized');
+  },
+  logout: () => {
+    throw new Error('UserContext is not initialized');
+  }
+});
+export const UserProvider: React.FC = ({ children }) => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  // This method updates the user state in the redux store
+  const updateUserState = (user: authUser) => {
+    if (user) {
+      dispatch(addUser({ user }));
+    }
+  };
+
+  // This method logs the user out and removes the JWT from the local storage
+  const logout = () => {
+    removeJWT();
+    dispatch(removeUser({}));
+    navigate('/login');
+  };
+
+  const values = {
+    logout,
+    updateUserState
+  };
+  return <UserContext.Provider value={values}>{children}</UserContext.Provider>;
+};
+
+export const useUser = (): UserContextTypes => {
+  const User = useContext(UserContext);
+  if (!User) {
+    throw new Error('useUser must be used within an UserProvider');
+  }
+  return User;
+};
