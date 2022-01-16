34eda6b4d3d6f348806247c28e57198e783dc6a1
JaFe <kontakt.jakubfedoszczak@gmail.com>
2022-01-16 05:24:32 +0100

    Add fitContent prop to Loader

---
+++ src/components/molecules/UserRecord/UserRecord.tsx
@@ -0,0 +1,69 @@
+import Input from '../../atoms/Input/Input';
+import { PeopleWrapper, Select } from './UserRecord.styles';
+import Button from '../../atoms/Button/Button';
+import { useForm } from 'react-hook-form';
+import { storeRoot, useAddUserToClassMutation } from '../../../store';
+import { getRoleFromText } from '../../../helpers/roles';
+import { useSelector } from 'react-redux';
+import React from 'react';
+import { useClass } from '../../../hooks/useClass';
+import { nanoid } from '@reduxjs/toolkit';
+import Loader from 'components/atoms/Loader/Loader';
+
+interface props {
+  index: number;
+}
+
+const UserRecord: React.FC<props> = ({ index: i }) => {
+  const {
+    register: registerUser,
+    formState: { errors },
+    handleSubmit: handleSaveUser
+  } = useForm();
+  const [addUser, { isLoading, isSuccess }] = useAddUserToClassMutation();
+  const user = useSelector((state: storeRoot) => state.user);
+  const { classId } = useClass();
+
+  const saveUser = (tempUser: any) => {
+    if (isLoading) return;
+    const dividedName = tempUser.name.split(' ');
+    tempUser.first_name = dividedName[0];
+    tempUser.last_name = dividedName[1];
+    const preparedUser = {
+      username: `${tempUser.first_name.toLowerCase()}_${tempUser.last_name.toLowerCase()}`,
+      email: `${nanoid()}@email.com`,
+      first_name: tempUser.first_name,
+      last_name: tempUser.last_name,
+      confirmed: true,
+      blocked: false,
+      birthday: tempUser.birthday,
+      avatar: null,
+      schoolId: user?.schoolId || null,
+      textRole: tempUser.TextRole,
+      role: getRoleFromText(tempUser?.TextRole || 'Student'),
+      class: classId,
+      password: nanoid()
+    };
+    addUser({ ...preparedUser });
+  };
+
+  return (
+    <PeopleWrapper as={'form'} onSubmit={handleSaveUser(saveUser)}>
+      <h1 style={{ color: isSuccess ? 'green' : undefined }}>{i + 1}.</h1>
+      <>
+        <Input type="text" placeholder="Imię i nazwisko" error={errors.name} {...registerUser('name', { required: true })} disabled={isSuccess} />
+        <Select {...registerUser('role', { required: true })} disabled={isSuccess}>
+          <option value="Student">Uczeń</option>
+          <option value="Moderator">Samorząd Uczniowski</option>
+          <option value="School Admin">Administrator</option>
+        </Select>
+        <Input type="date" placeholder="urodziny" error={errors.birthday} {...registerUser('birthday', { required: true })} disabled={isSuccess} />
+        <Button isIcon isDisabled={isSuccess}>
+          {!isLoading ? '+' : <Loader fitContent />}
+        </Button>
+      </>
+    </PeopleWrapper>
+  );
+};
+
+export default UserRecord;
