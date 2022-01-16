0618d57cd305e906ff5eb6dc1410dbc1322ba303
TeodorWolski <teodorwolski88@gmail.com>
2022-01-16 17:15:08 +0100

    Exporting GPR styles

---
+++ src/components/molecules/GoodPasswordRules/GoodPasswordRules.styles.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const PasswordInfoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 0rem;
+`;
+
+export const Signature = styled.p`
+  font-weight: ${({ theme }) => theme.fontWeight.medium};
+  font-size: ${({ theme }) => theme.fontSize.s};
+`;
+
+export const SignatureWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 1.2rem;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const PasswordInfoHeading = styled.h1`
+  font-size: ${({ theme }) => theme.fontSize.m};
+  transform: translateY(25%);
+  font-weight: ${({ theme }) => theme.fontWeight.semibold};
+`;
