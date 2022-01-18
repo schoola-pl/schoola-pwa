0801092bc6edcae2290d5b927d0b925e0c0ec1cc
TeodorWolski <teodorwolski88@gmail.com>
2022-01-18 10:28:35 +0100

    ClassName fix

--- src/components/molecules/ClassLink/ClassLink.tsx
+++ src/components/molecules/ClassLink/ClassLink.tsx
@@ -1,14 +1,18 @@
 import { InfoWrapper, Wrapper } from './ClassLink.styles';
 
 interface Props {
-  name: string;
+  classLevel: number;
+  classLetter: string;
   numberOfStudents: number;
 }
 
-const ClassLink: React.FC<Props> = ({ name, numberOfStudents }) => (
+const ClassLink: React.FC<Props> = ({ classLevel, classLetter, numberOfStudents }) => (
   <Wrapper to="#">
     <InfoWrapper>
-      <h1>{name}</h1>
+      <div>
+        <h1>{classLevel}</h1>
+        <h1>{classLetter}</h1>
+      </div>
       <p>
         liczba osób w klasie: <strong>{numberOfStudents}</strong>
       </p>
