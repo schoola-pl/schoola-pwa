d72c5047edfaab8a357d72fc6bc884967091fb04
JaFe <kontakt.jakubfedoszczak@gmail.com>
2022-01-19 16:54:02 +0100

    Add editable state

--- src/components/molecules/StudentDetail/StudentDetail.tsx
+++ src/components/molecules/StudentDetail/StudentDetail.tsx
@@ -1,27 +1,17 @@
-import EditIcon from 'assets/icons/EditIcon.png';
-import blueStudent from 'assets/icons/BlueStudent.svg';
-import DeleteIcon from 'assets/icons/DeleteIcon.svg';
-import { BoxWrapper, Date, DeleteBox, EditBox, Name, Number, Role, StudentBox, Wrapper } from './StudentDetails.styles';
 import React from 'react';
+import StudentInfoRecord from '../StudentInfoRecord/StudentInfoRecord';
 
 interface props {
-  students: { id: string; attributes: { first_name: string; last_name: string; blocked: boolean; avatar: string; Birthday: string } }[];
+  students: {
+    id: string;
+    attributes: { first_name: string; last_name: string; blocked: boolean; avatar: string; Birthday: string; TextRole: string };
+  }[];
 }
 
 const StudentDetail: React.FC<props> = ({ students }) => (
   <>
-    {students.map(({ id, attributes: { first_name, last_name, Birthday } }) => (
-      <Wrapper key={id}>
-        <StudentBox icon={blueStudent} />
-        <Name>{`${first_name} ${last_name}`}</Name>
-        <Role>Uczeń</Role>
-        <Date>{Birthday}</Date>
-        <Number>{id}</Number>
-        <BoxWrapper>
-          <EditBox icon={EditIcon} />
-          <DeleteBox icon={DeleteIcon} />
-        </BoxWrapper>
-      </Wrapper>
+    {students.map((info) => (
+      <StudentInfoRecord key={info.id} info={info} />
     ))}
   </>
 );
---
+++ src/components/molecules/StudentInfoRecord/StudentInfoRecord.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { BoxWrapper, Date, DeleteBox, EditBox, Name, Number, Role, StudentBox, Wrapper } from '../StudentDetail/StudentDetails.styles';
+import blueStudent from '../../../assets/icons/BlueStudent.svg';
+import EditIcon from '../../../assets/icons/EditIcon.png';
+import DeleteIcon from '../../../assets/icons/DeleteIcon.svg';
+import Input from '../../atoms/Input/Input';
+import { Select } from '../../../views/auth/SchoolAdmin/AddClass/AddClass.styles';
+
+interface props {
+  info: {
+    id: string;
+    attributes: {
+      first_name: string;
+      last_name: string;
+      TextRole: string;
+      Birthday: string;
+      blocked: boolean;
+    };
+  };
+}
+
+const StudentInfoRecord: React.FC<props> = ({
+  info: {
+    id,
+    attributes: { first_name, Birthday, last_name, TextRole, blocked }
+  }
+}) => {
+  const [isEdit, setEditState] = useState(false);
+
+  return (
+    <Wrapper>
+      <StudentBox icon={blueStudent} />
+      {!isEdit ? <Name>{`${first_name} ${last_name}`}</Name> : <Input placeholder={`${first_name} ${last_name}`} small />}
+      {!isEdit ? (
+        <Role>{TextRole === 'Student' ? 'Uczeń' : 'Samorząd Uczniowski'}</Role>
+      ) : (
+        <Select small>
+          <option value="Student">Uczeń</option>
+          <option value="Moderator">Samorząd Uczniowski</option>
+        </Select>
+      )}
+      {!isEdit ? <Date>{Birthday}</Date> : <Input type="date" small />}
+      <Number>{id}</Number>
+      <BoxWrapper>
+        <EditBox onClick={() => setEditState((prev) => !prev)} icon={EditIcon} />
+        <DeleteBox icon={DeleteIcon} />
+      </BoxWrapper>
+    </Wrapper>
+  );
+};
+
+export default StudentInfoRecord;
