7b47091cc3757028a0649741ff5b4180e56309fa
JaFe <kontakt.jakubfedoszczak@gmail.com>
2022-01-23 08:37:32 +0100

    Add react-hook-form to form

--- src/views/auth/Forms/EditProfile/EditProfile.tsx
+++ src/views/auth/Forms/EditProfile/EditProfile.tsx
@@ -1,25 +1,32 @@
 import { Card, CardHeading, EditProfileForm, Label, StyledInput, SubmitButton } from './EditProfile.styles';
 import { useSelector } from 'react-redux';
 import { storeRoot } from 'store';
+import { useForm } from 'react-hook-form';
+import { settingsType } from 'types/school';
 
 const EditProfile = () => {
   const user = useSelector((state: storeRoot) => state.user);
+  const { register, handleSubmit } = useForm();
+
+  const handleChangeSettings = (settings: settingsType) => {
+    console.log(settings);
+  };
 
   return (
     <div>
       <CardHeading>Edytuj profil</CardHeading>
-      <Card>
+      <Card as="form" onSubmit={handleSubmit(handleChangeSettings)}>
         <EditProfileForm>
           <Label>E-mail</Label>
-          <StyledInput placeholder={user?.email || 'Wczytywanie...'} />
+          <StyledInput placeholder={user?.email || 'Wczytywanie...'} {...register('email')} />
           <Label>Imię</Label>
-          <StyledInput placeholder={user?.first_name || 'Wczytywanie...'} />
+          <StyledInput placeholder={user?.first_name || 'Wczytywanie...'} {...register('first_name')} />
           <Label>Nazwisko</Label>
-          <StyledInput placeholder={user?.last_name || 'Wczytywanie...'} />
+          <StyledInput placeholder={user?.last_name || 'Wczytywanie...'} {...register('last_name')} />
           <Label>
             Data urodzenia (aktualna: <i>{user?.Birthday || 'Wczytywanie...'}</i>)
           </Label>
-          <StyledInput type="date" />
+          <StyledInput type="date" {...register('Birthday')} />
         </EditProfileForm>
         <SubmitButton>Zatwierdź</SubmitButton>
       </Card>
