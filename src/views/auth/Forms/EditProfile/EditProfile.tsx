39e168fbfe69be705dc7c1cd4460b3e36746638b
JaFe <kontakt.jakubfedoszczak@gmail.com>
2022-01-23 08:23:59 +0100

    Add getting and displaying old user data

--- src/views/auth/Forms/EditProfile/EditProfile.tsx
+++ src/views/auth/Forms/EditProfile/EditProfile.tsx
@@ -1,22 +1,30 @@
-import { CardHeading, Card, EditProfileForm, Label, StyledInput, SubmitButton } from './EditProfile.styles';
+import { Card, CardHeading, EditProfileForm, Label, StyledInput, SubmitButton } from './EditProfile.styles';
+import { useSelector } from 'react-redux';
+import { storeRoot } from 'store';
 
-const EditProfile = () => (
-  <div>
-    <CardHeading>Edytuj profil</CardHeading>
-    <Card>
-      <EditProfileForm>
-        <Label>E-mail</Label>
-        <StyledInput />
-        <Label>Imię</Label>
-        <StyledInput />
-        <Label>Nazwisko</Label>
-        <StyledInput />
-        <Label>Data urodzenia</Label>
-        <StyledInput type="date" />
-      </EditProfileForm>
-      <SubmitButton>Zatwierdź</SubmitButton>
-    </Card>
-  </div>
-);
+const EditProfile = () => {
+  const user = useSelector((state: storeRoot) => state.user);
+
+  return (
+    <div>
+      <CardHeading>Edytuj profil</CardHeading>
+      <Card>
+        <EditProfileForm>
+          <Label>E-mail</Label>
+          <StyledInput placeholder={user?.email || 'Wczytywanie...'} />
+          <Label>Imię</Label>
+          <StyledInput placeholder={user?.first_name || 'Wczytywanie...'} />
+          <Label>Nazwisko</Label>
+          <StyledInput placeholder={user?.last_name || 'Wczytywanie...'} />
+          <Label>
+            Data urodzenia (aktualna: <i>{user?.Birthday || 'Wczytywanie...'}</i>)
+          </Label>
+          <StyledInput type="date" />
+        </EditProfileForm>
+        <SubmitButton>Zatwierdź</SubmitButton>
+      </Card>
+    </div>
+  );
+};
 
 export default EditProfile;
