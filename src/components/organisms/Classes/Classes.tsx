f4479ad7ffe905c822a6ba66cd6f798353e5849e
JaFe <kontakt.jakubfedoszczak@gmail.com>
2022-01-28 16:50:41 +0100

    Remove classes counter to Classes component

--- src/components/organisms/Classes/Classes.tsx
+++ src/components/organisms/Classes/Classes.tsx
@@ -1,13 +1,25 @@
 import ClassCard from 'components/molecules/ClassCard/ClassCard';
-import { ClassesWrapper } from 'views/auth/SchoolAdmin/ManageClasses/ManageClasses.styles';
+import { AmountWrapper, ClassesWrapper } from 'views/auth/SchoolAdmin/ManageClasses/ManageClasses.styles';
+import { useSelector } from 'react-redux';
+import { storeRoot, useGetClassesCountQuery } from 'store';
 
 const Classes = () => {
+  const user = useSelector((store: storeRoot) => store.user);
+  const classesCount = useGetClassesCountQuery({ schoolId: user?.schoolId || null });
+
   return (
-    <ClassesWrapper>
-      <ClassCard classYear="Klasy pierwsze" classLevel={1} />
-      <ClassCard classYear="Klasy drugie" classLevel={2} />
-      <ClassCard classYear="Klasy trzecie" classLevel={3} />
-    </ClassesWrapper>
+    <>
+      <AmountWrapper>
+        <h1>
+          Łączna liczba klas: <span>{classesCount.isLoading ? 'Liczenie...' : classesCount.data.data.length}</span>
+        </h1>
+      </AmountWrapper>
+      <ClassesWrapper>
+        <ClassCard classYear="Klasy pierwsze" classLevel={1} />
+        <ClassCard classYear="Klasy drugie" classLevel={2} />
+        <ClassCard classYear="Klasy trzecie" classLevel={3} />
+      </ClassesWrapper>
+    </>
   );
 };
 
