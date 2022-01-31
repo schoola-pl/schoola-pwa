c8235ea1d85d0134083a5439ebdeb21884d57bfa
JaFe <kontakt.jakubfedoszczak@gmail.com>
2022-01-31 05:04:57 +0100

    Exclude styles from Loading component

--- src/components/molecules/Loading/Loading.tsx
+++ src/components/molecules/Loading/Loading.tsx
@@ -1,19 +1,6 @@
 import Loader from 'components/atoms/Loader/Loader';
 import React from 'react';
-import styled from 'styled-components';
-
-export const Wrapper = styled.div`
-  --loadingColor: white;
-  width: 100%;
-  min-height: 100%;
-  position: absolute;
-  left: 0;
-  top: 0;
-  background: var(--loadingColor);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
+import { Wrapper } from './Loading.styles';
 
 const Loading: React.FC = () => {
   return (
