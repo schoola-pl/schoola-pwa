a5ce4d366b8040508cb316fbdd601db7d613c2b4
JaFe <kontakt.jakubfedoszczak@gmail.com>
2022-02-02 05:16:47 +0100

    Fix typos in EditProfile

--- src/views/auth/Forms/EditProfile/EditProfile.tsx
+++ src/views/auth/Forms/EditProfile/EditProfile.tsx
@@ -34,6 +34,7 @@ const EditProfile = () => {
             error={errors.email}
             {...register('email', {
               pattern:
+                // eslint-disable-next-line
                 /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g
             })}
           />
