ecb204001b5eb5f5d25a63ca5ad1d53c92adfdec
TeodorWolski <teodorwolski88@gmail.com>
2022-02-02 13:41:42 +0100

    Exporting spotted styles

--- src/views/auth/User/Spotted/Spotted.tsx
+++ src/views/auth/User/Spotted/Spotted.tsx
@@ -1,15 +1,6 @@
 import UserTemplate from 'components/templates/UserTemplate/UserTemplate';
-import styled from 'styled-components';
+import { PageWrapper } from './Spotted.styles';
 import AskQuestionInput from 'components/molecules/AskQuestionInput/AskQuestionInput';
-
-export const PageWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100vw;
-  heigth: 100vh;
-  justify-content: flex-start;
-  align-items: center;
-`;
 
 const Spotted = () => {
   return (
