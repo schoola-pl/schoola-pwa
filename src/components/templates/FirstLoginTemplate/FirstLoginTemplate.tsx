7262a9eb35946a687713358943af309bbde3fbfe
JaFe <kontakt.jakubfedoszczak@gmail.com>
2022-02-04 05:46:17 +0100

    Add logic for changing slides

--- src/components/templates/FirstLoginTemplate/FirstLoginTemplate.tsx
+++ src/components/templates/FirstLoginTemplate/FirstLoginTemplate.tsx
@@ -1,14 +1,47 @@
-import { WelcomeButton, Wrapper } from './FirstLoginTemplate.styles';
+import { Animated, WelcomeButton, Wrapper } from './FirstLoginTemplate.styles';
 import StepCircles from 'components/molecules/StepCircles/StepCircles';
 import ArrowIcon from 'assets/icons/ArrowIcon.svg';
-import React from 'react';
+import React, { useState } from 'react';
+import Welcome from 'views/auth/User/FirstLoginPages/Welcome/Welcome';
+import DataPage from 'views/auth/User/FirstLoginPages/DataPage/DataPage';
+import Hobbies from 'views/auth/User/FirstLoginPages/Hobbies/Hobbies';
+import FinishPage from 'views/auth/User/FirstLoginPages/FinishPage/FinishPage';
 
-const FirstLoginTemplate: React.FC = ({ children }) => (
-  <Wrapper>
-    {children}
-    <WelcomeButton icon={ArrowIcon} />
-    <StepCircles />
-  </Wrapper>
-);
+const FirstLoginTemplate: React.FC = () => {
+  const [counter, setCounter] = useState(0);
+  const [isFinished, setIsFinished] = useState(false);
+
+  const nextStep = () => {
+    if (counter < 2) {
+      setCounter((prev) => ++prev);
+    } else {
+      setCounter((prev) => ++prev);
+      setIsFinished(true);
+    }
+  };
+
+  const decideStep = (c: number) => {
+    switch (c) {
+      case 0:
+        return <Welcome />;
+      case 1:
+        return <DataPage />;
+      case 2:
+        return <Hobbies />;
+      case 3:
+        return <FinishPage />;
+      default:
+        return <Welcome />;
+    }
+  };
+
+  return (
+    <Wrapper>
+      <Animated>{decideStep(counter)}</Animated>
+      {!isFinished && <WelcomeButton onClick={nextStep} icon={ArrowIcon} />}
+      {!isFinished && <StepCircles step={counter} />}
+    </Wrapper>
+  );
+};
 
 export default FirstLoginTemplate;
