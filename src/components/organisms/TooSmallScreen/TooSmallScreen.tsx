d2adc4d64f4649e4b626025947864ec49c0c2f02
JaFe <kontakt.jakubfedoszczak@gmail.com>
2022-02-06 16:45:29 +0100

    Edit text in TooSmallScreen component

--- src/components/organisms/TooSmallScreen/TooSmallScreen.tsx
+++ src/components/organisms/TooSmallScreen/TooSmallScreen.tsx
@@ -4,7 +4,7 @@ import { Wrapper } from './TooSmallScreen.styles';
 const TooSmallScreen: React.FC = () => {
   return (
     <Wrapper>
-      <h1>Aby móc korzystać z panelu administratora szkolnego, musisz mieć większy ekran!</h1>
+      <h1>Aby móc korzystać z panelu administratora szkolnego, musisz mieć większy ekran (np. komputer)!</h1>
       <p>Odśwież, aby załadować ponownie!</p>
     </Wrapper>
   );
