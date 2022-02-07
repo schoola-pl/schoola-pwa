7ff8a31806b02deb7017eb7ab6b6f60c87ef8459
JaFe <kontakt.jakubfedoszczak@gmail.com>
2022-02-07 11:18:52 +0100

    Add Intesteds API

---
+++ src/store/Api/Interesteds.ts
@@ -0,0 +1,32 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { getJWT } from 'helpers/jwt';
+
+export const InterestedsAPI = createApi({
+  reducerPath: 'InterestedsAPI',
+  baseQuery: fetchBaseQuery({
+    baseUrl: process.env.REACT_APP_BACKEND_BASE_URL
+  }),
+  endpoints: (builder) => ({
+    getInteresteds: builder.query<
+      {
+        data: {
+          id: number;
+          attributes: {
+            name: string;
+          };
+        }[];
+      },
+      unknown
+    >({
+      query: () => ({
+        url: `/interesteds`,
+        headers: {
+          Authorization: `Bearer ${getJWT()}`
+        }
+      })
+    })
+  })
+});
+
+export const { useGetInterestedsQuery } = InterestedsAPI;
+export default InterestedsAPI;
