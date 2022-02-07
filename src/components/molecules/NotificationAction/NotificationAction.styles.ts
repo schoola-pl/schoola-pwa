e3fc8737c3b3b3eb04c87737b7e70ab0fd4ed98b
TeodorWolski <teodorwolski88@gmail.com>
2022-02-07 14:31:22 +0100

    Cleaning notification action

---
+++ src/components/molecules/NotificationAction/NotificationAction.styles.ts
@@ -0,0 +1,73 @@
+import styled from 'styled-components';
+import SidebarLink from 'components/atoms/SidebarLink/SidebarLink';
+export const Form = styled.form`
+  transform: translateY(-10%);
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  height: 100%;
+  width: 100%;
+  list-style: none;
+`;
+
+export const NotificationContentWrapper = styled.div`
+  padding: 2rem;
+  width: 100%;
+  display: grid;
+  grid-template-columns: 20% 80%;
+  justify-items: center;
+  align-items: center;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.selectedItemGrey};
+
+  h1 {
+    text-align: left;
+    margin-right: 2rem;
+    font-size: ${({ theme }) => theme.fontSize.xs};
+    font-weight: ${({ theme }) => theme.fontWeight.semibold};
+  }
+
+  p {
+    margin-left: 5rem;
+    font-size: ${({ theme }) => theme.fontSize.xs};
+  }
+`;
+
+export const InfoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-left: 2rem;
+
+  h1 {
+    padding-left: 2rem;
+    text-align: center;
+  }
+`;
+
+export const BoxWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Box = styled(SidebarLink)`
+  border-radius: 1rem;
+  height: 3.5rem;
+  width: 3.5rem;
+  padding: 1rem;
+  opacity: 0.8;
+  transition: opacity 0.2s ease-in-out;
+  &:hover {
+    opacity: 1;
+  }
+`;
+
+export const CancelBox = styled(Box)`
+  background-color: #fcb3b0;
+`;
+
+export const EditBox = styled(Box)`
+  background-color: #e8fcd9;
+`;
