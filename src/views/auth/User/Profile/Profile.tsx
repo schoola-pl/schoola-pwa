cc65de448643749843ecb9ab23bd9b6664091a2f
TeodorWolski <teodorwolski88@gmail.com>
2022-02-11 23:00:10 +0100

    Creating first section of the profile view

--- src/views/auth/User/Profile/Profile.tsx
+++ src/views/auth/User/Profile/Profile.tsx
@@ -1,7 +1,9 @@
 import styled from 'styled-components';
 import UserTemplate from 'components/templates/UserTemplate/UserTemplate';
+import SidebarLink from 'components/atoms/SidebarLink/SidebarLink';
+import StudentHatIcon from 'assets/icons/StudentHatIcon.svg';
 
-const Wrapper = styled.div`
+export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   width: 100vw;
@@ -10,9 +12,8 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
-const Grid = styled.div`
+export const Grid = styled.div`
   transform: translateY(-10%);
-
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   grid-template-rows: auto;
@@ -25,7 +26,34 @@ const Grid = styled.div`
   }
 `;
 
-const UserInfoWrapper = styled.div`
+export const RoleWrapper = styled.div`
+  background-color: white;
+  box-shadow: -2px 4px 10px rgba(115, 124, 142, 0.09);
+  border-radius: 2rem;
+  height: 11rem;
+  width: 14rem;
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  padding-left: 1.5rem;
+
+  h1 {
+    font-size: ${({ theme }) => theme.fontSize.s};
+    font-weight: ${({ theme }) => theme.fontWeight.medium};
+    &::after {
+      content: '👨‍🎓';
+      padding-left: 1rem;
+    }
+  }
+
+  p {
+    transform: translateY(15%);
+    font-size: ${({ theme }) => theme.fontSize.s};
+    color: grey;
+  }
+`;
+
+export const UserInfoWrapper = styled.div`
   position: relative;
   overflow: hidden;
   display: flex;
@@ -33,34 +61,112 @@ const UserInfoWrapper = styled.div`
   align-items: center;
   justify-content: center;
   border-radius: 2rem;
-  width: 15rem;
-  height: 22rem;
+  width: 17rem;
+  height: 24rem;
   background-color: white;
   box-shadow: -2px 4px 10px rgba(115, 124, 142, 0.09);
 `;
 
-const InnerWrapper = styled.div`
+export const Picture = styled.img`
+  background-repeat: no-repeat;
+  max-height: 70%;
+  min-width: 100%;
+`;
+
+export const ContentWrapper = styled.div`
+  height: 30%;
+  width: 100%;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  padding-left: 1rem;
+  transform: translateY(-10%);
+
+  h1 {
+    font-size: ${({ theme }) => theme.fontSize.s};
+    font-weight: ${({ theme }) => theme.fontWeight.medium};
+  }
+
+  p {
+    transform: translateY(-60%);
+    color: grey;
+    font-size: ${({ theme }) => theme.fontSize.xs};
+    font-weight: ${({ theme }) => theme.fontWeight.light};
+  }
+`;
+
+export const ClassWrapper = styled.div`
   background-color: white;
   box-shadow: -2px 4px 10px rgba(115, 124, 142, 0.09);
   border-radius: 2rem;
-  width: 15rem;
+  height: 11rem;
+  width: 14rem;
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  flex-direction: column;
+  padding-left: 1.5rem;
+
+  h1 {
+    font-size: ${({ theme }) => theme.fontSize.m};
+    font-weight: ${({ theme }) => theme.fontWeight.medium};
+  }
+
+  p {
+    font-size: ${({ theme }) => theme.fontSize.s};
+    color: grey;
+    display: flex;
+    align-items: center;
+
+    &::after {
+      content: '>';
+      padding-left: 1rem;
+      font-size: ${({ theme }) => theme.fontSize.s};
+    }
+  }
 `;
 
-const Picture = styled.img`
-  background-size: ;
+const CirclesWrapper = styled.div`
+  display: flex;
+  transform: translateX(-15%);
+`;
+
+interface Props {
+  icon?: string;
+}
+
+const CircleOne = styled.img<Props>`
+  z-index: 2;
   background-repeat: no-repeat;
-  height: 75%;
+  height: 3.5rem;
+  width: 3.5rem;
+  border-radius: 3.5rem;
+  border: 3px solid white;
+  transform: translateX(64%);
+`;
+const CircleTwo = styled.img<Props>`
+  z-index: 1;
+  background-repeat: no-repeat;
+  height: 3.5rem;
+  width: 3.5rem;
+  border-radius: 3.5rem;
+  border: 3px solid white;
+  transform: translateX(32%);
+`;
+const CircleThree = styled.img<Props>`
+  z-index: 0;
+  background-repeat: no-repeat;
+  height: 3.5rem;
+  width: 3.5rem;
+  border-radius: 3.5rem;
+  border: 3px solid white;
 `;
 
-// const PictureWrapper = styled.div`
-
-// `;
-
-const ContentWrapper = styled.div`
-  height: 30%;
-  border-top: 1px solid black;
-  width: 100%;
-  background-color: white;
+const Flex = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  transform: translateY(-25%);
 `;
 
 const Profile = () => {
@@ -68,16 +174,33 @@ const Profile = () => {
     <UserTemplate>
       <Wrapper>
         <Grid>
-          <InnerWrapper>Klasa</InnerWrapper>
-          <InnerWrapper>Rola</InnerWrapper>
+          <ClassWrapper>
+            <div>
+              <p>Klasa</p>
+              <Flex>
+                <h1>3B</h1>
+                <CirclesWrapper>
+                  <CircleOne src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fbi.gazeta.pl%2Fim%2Ffd%2F2f%2F16%2Fz23263741IH%2CMarcin-Najman.jpg&f=1&nofb=1" />
+                  <CircleTwo src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimg.dorzeczy.pl%2Fimg%2Fkrzysztof-stanowski%2F99%2F19%2F6efea9df1d0f022543fd5ff5b60b.jpeg&f=1&nofb=1" />
+                  <CircleThree src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fbi.im-g.pl%2Fim%2F8c%2F48%2F18%2Fz25462668IER%2CMalgorzata-Rozenek---Majdan.jpg&f=1&nofb=1" />
+                </CirclesWrapper>
+              </Flex>
+            </div>
+          </ClassWrapper>
+          <RoleWrapper>
+            <div>
+              <p>Rola:</p>
+              <h1>Uczeń</h1>
+            </div>
+          </RoleWrapper>
           <UserInfoWrapper>
             <Picture
-              src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fd.wpimg.pl%2F1861132436-573487822%2Fkuba-wojewodzki.jpg&f=1&nofb=1"
-              alt=""
+              src="https://s7.tvp.pl/images2/7/8/e/uid_78ea317e92356f6cd75b0ad95c3fbd7e1634707736779_width_1280_play_0_pos_0_gs_0_height_720_kuba-wojewodzki-fot-papmarcin-kmiecinski.jpg"
+              alt="user photo"
             />
-
             <ContentWrapper>
-              <p>hello</p>
+              <h1>Kuba Wojewódźki</h1>
+              <p>Lorem ipsum dolor sit amet</p>
             </ContentWrapper>
           </UserInfoWrapper>
         </Grid>
