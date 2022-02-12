bd2fd64d88e2ee58fea5fdc9bf3cffc4ffe5ed1b
TeodorWolski <teodorwolski88@gmail.com>
2022-02-12 20:50:22 +0100

    Static version of Profile view

---
+++ src/components/molecules/ProfileClass/ProfileClass.tsx
@@ -0,0 +1,19 @@
+import { ClassWrapper, Flex, CirclesWrapper, CircleOne, CircleTwo, CircleThree } from './ProfileClass.styles';
+
+const ProfileClass = () => (
+  <ClassWrapper>
+    <div>
+      <p>Klasa</p>
+      <Flex>
+        <h1>3B</h1>
+        <CirclesWrapper>
+          <CircleOne src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fbi.gazeta.pl%2Fim%2Ffd%2F2f%2F16%2Fz23263741IH%2CMarcin-Najman.jpg&f=1&nofb=1" />
+          <CircleTwo src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimg.dorzeczy.pl%2Fimg%2Fkrzysztof-stanowski%2F99%2F19%2F6efea9df1d0f022543fd5ff5b60b.jpeg&f=1&nofb=1" />
+          <CircleThree src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fbi.im-g.pl%2Fim%2F8c%2F48%2F18%2Fz25462668IER%2CMalgorzata-Rozenek---Majdan.jpg&f=1&nofb=1" />
+        </CirclesWrapper>
+      </Flex>
+    </div>
+  </ClassWrapper>
+);
+
+export default ProfileClass;
--- src/views/auth/User/Profile/Profile.tsx
+++ src/views/auth/User/Profile/Profile.tsx
@@ -4,6 +4,11 @@ import Person from 'components/atoms/Person/Person';
 import Interests from 'components/atoms/Interests/Interests';
 import Roles from 'components/atoms/Roles/Roles';
 import ProfileClass from 'components/molecules/ProfileClass/ProfileClass';
+import SpotifyIcon from 'assets/icons/SocialMediaIcons/SpotifyIcon.svg';
+import InstagramIcon from 'assets/icons/SocialMediaIcons/InstagramIcon.svg';
+import GithubIcon from 'assets/icons/SocialMediaIcons/GithubIcon.svg';
+import FacebookIcon from 'assets/icons/SocialMediaIcons/FacebookIcon.svg';
+import WebsiteIcon from 'assets/icons/SocialMediaIcons/WebsiteIcon.svg';
 
 export const Wrapper = styled.div`
   display: flex;
@@ -28,6 +33,42 @@ export const Grid = styled.div`
   }
 `;
 
+const LinkWrapper = styled.div`
+  display: flex;
+  text-align: left;
+  width: 100%;
+  flex-direction: column;
+  transform: translateY(-40%);
+`;
+
+interface Props {
+  icon: string;
+}
+
+const SocialMediaLink = styled.a<Props>`
+  height: 4rem;
+  width: 4rem;
+  background-image: url(${({ icon }) => icon});
+  background-repeat: no-repeat;
+  background-color: transparent;
+  background-size: 75%;
+  background-position: center;
+  cursor: pointer;
+  border: none;
+  display: block;
+`;
+
+const SocialMediaWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+`;
+
+const LinksHeading = styled.h1`
+  padding-left: 2.5rem;
+  font-weight: ${({ theme }) => theme.fontWeight.medium};
+`;
+
 const Profile = () => {
   return (
     <UserTemplate>
@@ -38,6 +79,16 @@ const Profile = () => {
           <Person />
         </Grid>
         <Interests />
+        <LinkWrapper>
+          <LinksHeading>Linki</LinksHeading>
+          <SocialMediaWrapper>
+            <SocialMediaLink target="_blank" rel="noopener noreferrer" href="https://spotify.com" icon={SpotifyIcon} />
+            <SocialMediaLink target="_blank" rel="noopener noreferrer" href="https://instagram.com" icon={InstagramIcon} />
+            <SocialMediaLink target="_blank" rel="noopener noreferrer" href="https://github.com" icon={GithubIcon} />
+            <SocialMediaLink target="_blank" rel="noopener noreferrer" href="https://facebook.com" icon={FacebookIcon} />
+            <SocialMediaLink target="_blank" rel="noopener noreferrer" href="https://website.com" icon={WebsiteIcon} />
+          </SocialMediaWrapper>
+        </LinkWrapper>
       </Wrapper>
     </UserTemplate>
   );
