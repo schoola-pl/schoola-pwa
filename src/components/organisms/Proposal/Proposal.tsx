e667b6df2cb04785247d3dfb7c6330cfce7b29a7
JaFe <kontakt.jakubfedoszczak@gmail.com>
2022-02-13 07:50:54 +0100

    Add better UI for Proposal component

--- src/components/organisms/Proposal/Proposal.tsx
+++ src/components/organisms/Proposal/Proposal.tsx
@@ -1,27 +1,7 @@
-import styled from 'styled-components';
 import React from 'react';
 import { useSpotted } from 'hooks/useSpotted';
-
-export const Wrapper = styled.div`
-  position: relative;
-  display: grid;
-  min-height: 125px;
-  grid-template-rows: 7rem 1fr 4rem;
-  grid-gap: 1rem;
-  align-items: center;
-  background-color: white;
-  box-shadow: -2px 4px 10px rgba(115, 124, 142, 0.09);
-  border-radius: 1.5rem;
-  padding: 1rem;
-  width: 100%;
-
-  &:not(:last-child) {
-    margin-bottom: 3rem;
-  }
-`;
-
-export const Question = styled.p``;
-export const Decision = styled.div``;
+import Button from 'components/atoms/Button/Button';
+import { Decision, Question, Wrapper } from './Proposal.styles';
 
 interface props {
   qId: number;
@@ -36,15 +16,15 @@ const Proposal: React.FC<props> = ({ qId, question, resetSpotted }) => {
     <Wrapper>
       <Question>{question}</Question>
       <Decision>
-        <p
+        <Button
           onClick={async () => {
             await approveSpott(qId, question);
             resetSpotted();
           }}
         >
-          yes
-        </p>
-        <p onClick={() => disapproveSpott(qId)}>no</p>
+          Dodaj
+        </Button>
+        <Button onClick={() => disapproveSpott(qId)}>Usuń</Button>
       </Decision>
     </Wrapper>
   );
