2be87a939d3bacf857736938020e03256ab1c251
TeodorWolski <teodorwolski88@gmail.com>
2022-02-17 19:46:35 +0100

    Cleaning DropdownSelect

--- src/components/atoms/DropdownSelect/DropdownSelect.tsx
+++ src/components/atoms/DropdownSelect/DropdownSelect.tsx
@@ -1,37 +1,8 @@
-import styled from 'styled-components';
+import { StyledButton, StyledList } from './DropdownSelect.styles';
 /* eslint-disable react/jsx-no-comment-textnodes */
 import { useSelect } from 'downshift';
 
 const items: string[] = ['8:00', '8:55', '9:50', '10:55', '11:50', '12:45', '13:40', '14:30', '15:20', '16:10'];
-
-const StyledButton = styled.button`
-  border: none;
-  font-size: ${({ theme }) => theme.fontSize.s};
-  background-color: white;
-  border-radius: 1rem;
-  border: 2px solid ${({ theme }) => theme.colors.selectedItemGrey};
-`;
-
-const StyledList = styled.ul`
-  position: absolute;
-  background-color: white;
-  width: 8rem;
-  box-shadow: -2px 4px 10px rgba(115, 124, 142, 0.09);
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center !important;
-  border-radius: 1rem;
-  list-style: none;
-  overflow-y: scroll;
-
-  li {
-    width: 100%;
-    padding-bottom: 1rem;
-    transform: translateX(-55%);
-    font-size: ${({ theme }) => theme.fontSize.s};
-  }
-`;
 
 const stateReducer = (state: any, actionAndChanges: any) => {
   const { type, changes } = actionAndChanges;
