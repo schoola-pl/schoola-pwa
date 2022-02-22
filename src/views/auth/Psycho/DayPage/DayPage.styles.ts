fd30a1b7702f65110fc7294e53b079f92dbac817
TeodorWolski <teodorwolski88@gmail.com>
2022-02-22 20:58:13 +0100

    Exporting DayPage

---
+++ src/views/auth/Psycho/DayPage/DayPage.styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const PageWrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`;
+export const MeetingWrapper = styled.div`
+  display: flex;
+  width: 90%;
+  flex-direction: column;
+  align-items: center;
+`;
