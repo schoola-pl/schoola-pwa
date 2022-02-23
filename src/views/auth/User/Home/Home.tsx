928970aa49bac1f2b67ab7e85905be8b7bc4bd2a
JaFe <kontakt.jakubfedoszczak@gmail.com>
2022-02-23 18:46:27 +0100

    Add UI to home

--- src/views/auth/User/Home/Home.tsx
+++ src/views/auth/User/Home/Home.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Grid, Wrapper } from 'views/auth/User/Profile/Profile.styles';
+import ProfileClass from 'components/molecules/ProfileClass/ProfileClass';
+import Roles from 'components/atoms/Roles/Roles';
+import Person from 'components/atoms/Person/Person';
+import Interests from 'components/atoms/Interests/Interests';
+import Links from 'components/molecules/Links/Links';
 
 const Home: React.FC = () => {
-  return <p>For everyone</p>;
+  return (
+    <Wrapper>
+      <Grid>
+        <ProfileClass userClass={'4A'} />
+        <Roles role={'Student'} />
+        <Person userName={'jakub_fedoszczak'} userProfilePicture={''} description={'Opis usera 122'} />
+      </Grid>
+      <Interests interests={['fdsdf']} />
+      <Links socials={['dsfdas']} />
+    </Wrapper>
+  );
 };
 
 export default Home;
