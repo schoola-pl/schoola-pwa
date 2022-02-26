a82234167125c3824950b76ebb0526a54152d4c4
JaFe <kontakt.jakubfedoszczak@gmail.com>
2022-02-26 08:44:32 +0100

    Add FLP displaying

--- src/views/auth/Psycho/PsychoRoutes/PsychoRoutes.tsx
+++ src/views/auth/Psycho/PsychoRoutes/PsychoRoutes.tsx
@@ -1,21 +1,29 @@
-import { Routes } from 'react-router-dom';
-import { Navigate, Route } from 'react-router';
-import TodayPage from 'views/auth/Psycho/TodayPage/TodayPage';
+import PsychoTemplate from 'components/templates/PsychoTemplate/PsychoTemplate';
+import { useSelector } from 'react-redux';
+import { storeRoot } from 'store';
+import PsychoLoginTemplate from 'components/templates/PsychoLoginTemplate/PsychoLoginTemplate';
+import { Navigate, Route, Routes } from 'react-router';
 import ProfilePage from 'views/auth/Psycho/ProfilePage/ProfilePage';
 import CalendarPage from 'views/auth/Psycho/CalendarPage/CalendarPage';
+import TodayPage from 'views/auth/Psycho/TodayPage/TodayPage';
 import WeekPage from 'views/auth/Psycho/WeekPage/WeekPage';
-import PsychoTemplate from 'components/templates/PsychoTemplate/PsychoTemplate';
 
 const PsychoRoutes = () => {
+  const user = useSelector((state: storeRoot) => state.user);
+
   return (
     <PsychoTemplate>
-      <Routes>
-        <Route path="/" element={<Navigate to="today" />} />
-        <Route path="/today" element={<TodayPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/calendar" element={<CalendarPage />} />
-        <Route path="/week" element={<WeekPage />} />
-      </Routes>
+      {user && !user.confirmed ? (
+        <PsychoLoginTemplate />
+      ) : (
+        <Routes>
+          <Route path="/" element={<Navigate to="today" />} />
+          <Route path="/today" element={<TodayPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/calendar" element={<CalendarPage />} />
+          <Route path="/week" element={<WeekPage />} />
+        </Routes>
+      )}
     </PsychoTemplate>
   );
 };
