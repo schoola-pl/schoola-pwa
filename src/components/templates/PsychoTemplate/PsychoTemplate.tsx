f339bc228f6573a466971afff3da6ebf1dc529de
JaFe <kontakt.jakubfedoszczak@gmail.com>
2022-02-27 14:12:15 +0100

    Add cosmetic changes

--- src/components/templates/PsychoTemplate/PsychoTemplate.tsx
+++ src/components/templates/PsychoTemplate/PsychoTemplate.tsx
@@ -3,11 +3,8 @@ import PsychoSidebar from 'components/organisms/PsychoSidebar/PsychoSidebar';
 import PsychoTopBar from 'components/organisms/PsychoTopBar/PsychoTopBar';
 
 const Wrapper = styled.div`
-  height: 70vh;
-  width: 95%;
   margin-top: 10rem;
   z-index: -1;
-  overflow-x: hidden;
   overflow-y: auto !important;
 
   @media (min-height: 740px) {
--- src/views/auth/Psycho/WeekPage/WeekPage.tsx
+++ src/views/auth/Psycho/WeekPage/WeekPage.tsx
@@ -1,48 +1,45 @@
-import { PageWrapper, MeetingWrapper, Week, WeekWrapper, DayLink } from './WeekPage.styles';
-import PsychoTemplate from 'components/templates/PsychoTemplate/PsychoTemplate';
+import { DayLink, MeetingWrapper, PageWrapper, Week, WeekWrapper } from './WeekPage.styles';
 
 const WeekPage = () => (
-  <PsychoTemplate>
-    <PageWrapper>
-      <Week>
-        <WeekWrapper>
-          <p>13.02 - 20.02</p>
-        </WeekWrapper>
-      </Week>
-      <MeetingWrapper>
-        <DayLink to="/psycho/week/week-day">
-          <h1>Poniedziałek</h1>
-          <p>
-            liczba spotkań - <strong>5</strong>
-          </p>
-        </DayLink>
-        <DayLink to="/psycho/week/week-day">
-          <h1>Wtorek</h1>
-          <p>
-            liczba spotkań - <strong>3</strong>
-          </p>
-        </DayLink>
-        <DayLink to="/psycho/week/week-day">
-          <h1>Środa</h1>
-          <p>
-            liczba spotkań - <strong>4</strong>
-          </p>
-        </DayLink>
-        <DayLink to="/psycho/week/week-day">
-          <h1>Czwartek</h1>
-          <p>
-            liczba spotkań - <strong>2</strong>
-          </p>
-        </DayLink>
-        <DayLink to="/psycho/week/week-day">
-          <h1>Piątek</h1>
-          <p>
-            liczba spotkań - <strong>1</strong>
-          </p>
-        </DayLink>
-      </MeetingWrapper>
-    </PageWrapper>
-  </PsychoTemplate>
+  <PageWrapper>
+    <Week>
+      <WeekWrapper>
+        <p>13.02 - 20.02</p>
+      </WeekWrapper>
+    </Week>
+    <MeetingWrapper>
+      <DayLink to="/psycho/week/week-day">
+        <h1>Poniedziałek</h1>
+        <p>
+          liczba spotkań - <strong>5</strong>
+        </p>
+      </DayLink>
+      <DayLink to="/psycho/week/week-day">
+        <h1>Wtorek</h1>
+        <p>
+          liczba spotkań - <strong>3</strong>
+        </p>
+      </DayLink>
+      <DayLink to="/psycho/week/week-day">
+        <h1>Środa</h1>
+        <p>
+          liczba spotkań - <strong>4</strong>
+        </p>
+      </DayLink>
+      <DayLink to="/psycho/week/week-day">
+        <h1>Czwartek</h1>
+        <p>
+          liczba spotkań - <strong>2</strong>
+        </p>
+      </DayLink>
+      <DayLink to="/psycho/week/week-day">
+        <h1>Piątek</h1>
+        <p>
+          liczba spotkań - <strong>1</strong>
+        </p>
+      </DayLink>
+    </MeetingWrapper>
+  </PageWrapper>
 );
 
 export default WeekPage;
