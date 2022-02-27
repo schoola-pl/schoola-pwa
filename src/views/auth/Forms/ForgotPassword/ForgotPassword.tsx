2fd9170b59ab62bc79e18211044e351a36d024cb
JaFe <kontakt.jakubfedoszczak@gmail.com>
2022-02-27 05:45:24 +0100

    Add form validation

--- src/views/auth/Forms/ForgotPassword/ForgotPassword.tsx
+++ src/views/auth/Forms/ForgotPassword/ForgotPassword.tsx
@@ -1,18 +1,42 @@
-import { Wrapper, StyledLogo, Form, StyledInput, StyledLink } from './ForgotPassword.styles';
+import { Form, StyledInput, StyledLink, StyledLogo, Wrapper } from './ForgotPassword.styles';
 import Button from 'components/atoms/Button/Button';
 import AuthCard from 'components/molecules/AuthCard/AuthCard';
+import { useForm } from 'react-hook-form';
+import { useState } from 'react';
+import ErrorParagraph from 'components/atoms/ErrorParagraph/ErrorParagraph';
 
-const ForgotPassword = () => (
-  <Wrapper>
-    <AuthCard>
-      <StyledLogo />
-      <Form>
-        <StyledInput placeholder="Podaj swój email" />
-        <Button>Potwierdź</Button>
-        <StyledLink to="/login">Masz już konto?</StyledLink>
-      </Form>
-    </AuthCard>
-  </Wrapper>
-);
+const ForgotPassword = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
+  const [error, setError] = useState<number | null>(null);
+
+  const handleRestoreEmail = ({ email }: { email: string }) => {
+    console.log(email);
+  };
+
+  return (
+    <Wrapper>
+      <AuthCard>
+        <StyledLogo />
+        <Form onSubmit={handleSubmit(handleRestoreEmail)}>
+          <StyledInput
+            placeholder="Podaj swój email"
+            {...register('email', {
+              required: true,
+              pattern:
+                /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g
+            })}
+          />
+          {errors.email && <ErrorParagraph style={{ marginBottom: '2rem' }}>Podaj prawidłowy adres email!</ErrorParagraph>}
+          <Button>Potwierdź</Button>
+          <StyledLink to="/login">Masz już konto?</StyledLink>
+        </Form>
+      </AuthCard>
+    </Wrapper>
+  );
+};
 
 export default ForgotPassword;
