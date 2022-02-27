0005f1ce49478956b20bf42cf0dd219c53296633
JaFe <kontakt.jakubfedoszczak@gmail.com>
2022-02-27 19:27:21 +0100

    Add info about week

--- src/views/auth/Psycho/WeekPage/WeekPage.tsx
+++ src/views/auth/Psycho/WeekPage/WeekPage.tsx
@@ -1,45 +1,53 @@
 import { DayLink, MeetingWrapper, PageWrapper, Week, WeekWrapper } from './WeekPage.styles';
+import { endOfWeek, format, startOfWeek } from 'date-fns';
 
-const WeekPage = () => (
-  <PageWrapper>
-    <Week>
-      <WeekWrapper>
-        <p>13.02 - 20.02</p>
-      </WeekWrapper>
-    </Week>
-    <MeetingWrapper>
-      <DayLink to="/psycho/week/week-day">
-        <h1>Poniedziałek</h1>
-        <p>
-          liczba spotkań - <strong>5</strong>
-        </p>
-      </DayLink>
-      <DayLink to="/psycho/week/week-day">
-        <h1>Wtorek</h1>
-        <p>
-          liczba spotkań - <strong>3</strong>
-        </p>
-      </DayLink>
-      <DayLink to="/psycho/week/week-day">
-        <h1>Środa</h1>
-        <p>
-          liczba spotkań - <strong>4</strong>
-        </p>
-      </DayLink>
-      <DayLink to="/psycho/week/week-day">
-        <h1>Czwartek</h1>
-        <p>
-          liczba spotkań - <strong>2</strong>
-        </p>
-      </DayLink>
-      <DayLink to="/psycho/week/week-day">
-        <h1>Piątek</h1>
-        <p>
-          liczba spotkań - <strong>1</strong>
-        </p>
-      </DayLink>
-    </MeetingWrapper>
-  </PageWrapper>
-);
+const WeekPage = () => {
+  const weekStart = format(startOfWeek(new Date(), { weekStartsOn: 1 }), 'dd.MM');
+  const weekEnd = format(endOfWeek(new Date(), { weekStartsOn: 1 }), 'dd.MM');
+
+  return (
+    <PageWrapper>
+      <Week>
+        <WeekWrapper>
+          <p>
+            {weekStart} - {weekEnd}
+          </p>
+        </WeekWrapper>
+      </Week>
+      <MeetingWrapper>
+        <DayLink to="/psycho/week/week-day">
+          <h1>Poniedziałek</h1>
+          <p>
+            liczba spotkań - <strong>5</strong>
+          </p>
+        </DayLink>
+        <DayLink to="/psycho/week/week-day">
+          <h1>Wtorek</h1>
+          <p>
+            liczba spotkań - <strong>3</strong>
+          </p>
+        </DayLink>
+        <DayLink to="/psycho/week/week-day">
+          <h1>Środa</h1>
+          <p>
+            liczba spotkań - <strong>4</strong>
+          </p>
+        </DayLink>
+        <DayLink to="/psycho/week/week-day">
+          <h1>Czwartek</h1>
+          <p>
+            liczba spotkań - <strong>2</strong>
+          </p>
+        </DayLink>
+        <DayLink to="/psycho/week/week-day">
+          <h1>Piątek</h1>
+          <p>
+            liczba spotkań - <strong>1</strong>
+          </p>
+        </DayLink>
+      </MeetingWrapper>
+    </PageWrapper>
+  );
+};
 
 export default WeekPage;
