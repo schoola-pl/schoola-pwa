470ff917677f02fac52320264e48aa067e3d876b
TeodorWolski <teodorwolski88@gmail.com>
2022-02-27 13:19:47 +0100

    Day issue'

--- src/views/auth/Psycho/CalendarPage/CalendarPage.tsx
+++ src/views/auth/Psycho/CalendarPage/CalendarPage.tsx
@@ -2,12 +2,8 @@ import styled from 'styled-components';
 import { useState } from 'react';
 import Calendar from 'react-calendar';
 import PsychoTemplate from 'components/templates/PsychoTemplate/PsychoTemplate';
-import DateTimePicker from 'react-datetime-picker/dist/entry.nostyle';
 import './styles.css';
 
-{
-  /* <DateTimePicker calendarClassName onChange={onChange} value={value} /> */
-}
 export const PageWrapper = styled.div`
   width: 100vw;
   display: flex;
@@ -18,7 +14,7 @@ export const PageWrapper = styled.div`
 
 const StyledCalendar = styled(Calendar)`
   width: 90%;
-  height: 28rem;
+  height: 29.5rem;
   background-color: white;
   display: flex;
   flex-direction: column;
@@ -26,30 +22,85 @@ const StyledCalendar = styled(Calendar)`
   border-radius: 2rem;
   overflow: hidden;
   box-shadow: -2px 4px 10px rgba(115, 124, 142, 0.09);
+  margin-bottom: 1rem;
+`;
 
-  // button {
-  //   border: none;
-  //   padding: 1.5rem;
-  //   color: black;
-  //   background-color: white;
-  //   border-radius: 20rem;
-  // }
+const InnerWrapper = styled.div`
+  width: 90%;
+  height: 24.5rem;
+  background-color: white;
+  box-shadow: -2px 4px 10px rgba(115, 124, 142, 0.09);
+  border-radius: 2rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h1 {
+    margin-left: 3rem;
+    width: 100%;
+    font-weight: ${({ theme }) => theme.fontWeight.medium};
+  }
+
+  div {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 2.5rem;
+    margin-bottom: 0.2rem;
+    transform: translateY(-5%);
+  }
+
+  button {
+    border: none;
+    border-radius: 1.5rem;
+    color: white;
+    height: 4rem;
+    width: 90%;
+    background-color: ${({ theme }) => theme.colors.accentBlue};
+  }
+`;
+const Hour = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  border-radius: 1rem;
+  background-color: #f7f8fa;
+  height: 2.8rem;
+  width: 13rem;
+
+  font-size: ${({ theme }) => theme.fontSize.xs};
+  box-shadow: -2px 4px 10px rgba(115, 124, 142, 0.09);
 `;
 
 const CalendarPage = () => {
   const [value, onChange] = useState(new Date());
-
+  const setActive = () => {
+    const element: any = document.querySelector('.react-calendar__month-view__days__day');
+    element.classList.add('button-active');
+  };
   return (
     <PsychoTemplate>
       <PageWrapper>
         <StyledCalendar
-          // onClickDay={setActive}
+          onClickDay={() => setActive()}
           locale="pl"
           minDate={new Date(2022, 1, 1)}
           maxDate={new Date(2022, 6, 12)}
           onChange={onChange}
           value={value}
         />
+        <InnerWrapper>
+          <h1>Godziny</h1>
+          <div>
+            <Hour>8:00</Hour>
+            <Hour>8:55</Hour>
+            <Hour>9:50</Hour>
+            <Hour>10:55</Hour>
+            <Hour>11:50</Hour>
+            <Hour>12:35</Hour>
+          </div>
+          <button>Potwierdź</button>
+        </InnerWrapper>
       </PageWrapper>
     </PsychoTemplate>
   );
