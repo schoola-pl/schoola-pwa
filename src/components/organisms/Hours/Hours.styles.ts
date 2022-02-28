2359c26e3ac7d1938d508d8ee1dff079910ff5d4
TeodorWolski <teodorwolski88@gmail.com>
2022-02-28 18:10:10 +0100

    Applying mock data

--- src/components/organisms/Hours/Hours.styles.ts
+++ src/components/organisms/Hours/Hours.styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const Hour = styled.div`
+export const Hour = styled.div<{ isCanceled?: boolean }>`
   display: flex;
   align-items: center;
   justify-content: space-around;
@@ -11,6 +11,7 @@ export const Hour = styled.div`
   width: 13rem;
   font-size: ${({ theme }) => theme.fontSize.xs};
   box-shadow: -2px 4px 10px rgba(115, 124, 142, 0.09);
+  text-decoration: ${({ isCanceled }) => (isCanceled ? 'line-through' : 'none')};
 
   button {
     border: none;
@@ -20,6 +21,13 @@ export const Hour = styled.div`
     height: 2.3rem;
     width: 2.3rem;
     background-color: #fcb3b0;
+
+    &::after {
+      content: 'x';
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
   }
 `;
 export const HoursWrapper = styled.div`
--- src/components/organisms/Hours/Hours.tsx
+++ src/components/organisms/Hours/Hours.tsx
@@ -1,26 +1,22 @@
+import { useState } from 'react';
 import { Hour, HoursWrapper } from './Hours.styles';
 
+const hours = ['8:00', '8:55', '9:50', '10:55', '11:50', '12:35'];
+
 const Hours = () => {
+  const [isCanceled, setCancel] = useState(false);
+
+  const handleCancelHour = () => {
+    setCancel(!isCanceled);
+  };
+
   return (
     <HoursWrapper>
-      <Hour>
-        8:00 <button>x</button>
-      </Hour>
-      <Hour>
-        8:55 <button>x</button>
-      </Hour>
-      <Hour>
-        9:50 <button>x</button>
-      </Hour>
-      <Hour>
-        10:55 <button>x</button>
-      </Hour>
-      <Hour>
-        11:50 <button>x</button>
-      </Hour>
-      <Hour>
-        12:35 <button>x</button>
-      </Hour>
+      {hours.map((hour) => (
+        <Hour>
+          {hour} <button />
+        </Hour>
+      ))}
     </HoursWrapper>
   );
 };
