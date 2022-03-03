bcca3671bc33d3abfb92bfa3aafad906ab6287e1
JaFe <kontakt.jakubfedoszczak@gmail.com>
2022-03-03 18:58:50 +0100

    Modify useGetMeetings method

--- src/store/Api/Meetings.ts
+++ src/store/Api/Meetings.ts
@@ -1,6 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { getJWT } from 'helpers/jwt';
 import { multiResponse, strapiRequestType } from 'types/strapi';
+import { authUser } from 'types/auth';
 
 export const MeetingsAPI = createApi({
   reducerPath: 'MeetingsAPI',
@@ -8,19 +9,23 @@ export const MeetingsAPI = createApi({
     baseUrl: process.env.REACT_APP_BACKEND_BASE_URL
   }),
   endpoints: (builder) => ({
-    getMeetingsForDay: builder.query<{ id: number; date: string; isDone: boolean; start: string }[], { pId: strapiRequestType; date: string }>({
-      transformResponse: (response: multiResponse<{ date: string; isDone: boolean; start: string }>) => {
+    getMeetingsForDay: builder.query<
+      { id: number; date: string; isDone: boolean; start: string; user: authUser }[],
+      { pId: strapiRequestType; date: string }
+    >({
+      transformResponse: (response: multiResponse<{ date: string; isDone: boolean; start: string; user: { data: { attributes: authUser } } }>) => {
         const data = response.data;
         return data.map((item) => {
           const { id, attributes } = item;
           return {
             id,
-            ...attributes
+            ...attributes,
+            user: attributes.user.data.attributes
           };
         });
       },
       query: (args) => ({
-        url: `/mettings?filters[pId][$eq]=${args.pId}&filters[date][$eq]=${args.date}`,
+        url: `/mettings?filters[pId][$eq]=${args.pId}&filters[date][$eq]=${args.date}&populate=*`,
         headers: {
           Authorization: `Bearer ${getJWT()}`
         }
