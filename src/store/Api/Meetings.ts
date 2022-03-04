01bb3ad6654fed598c934e35de2447b7c62a974f
JaFe <kontakt.jakubfedoszczak@gmail.com>
2022-03-04 19:05:03 +0100

    Fix bug with optional date parameter

--- src/store/Api/Meetings.ts
+++ src/store/Api/Meetings.ts
@@ -34,7 +34,7 @@ export const MeetingsAPI = createApi({
     getMeetingsCount: builder.query<number, { pId: strapiRequestType; date?: string }>({
       transformResponse: (response: multiResponse) => response.data.length,
       query: (args) => ({
-        url: `/mettings?filters[pId][$eq]=${args.pId}&fields[0]=id${args.date && `&filters[date][$eq]=${args.date}`}`,
+        url: `/mettings?filters[pId][$eq]=${args.pId}&fields[0]=id${args.date ? `&filters[date][$eq]=${args.date}` : ''}`,
         headers: {
           Authorization: `Bearer ${getJWT()}`
         }
