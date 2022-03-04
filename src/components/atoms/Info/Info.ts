b55e8961587c8676659e6f1ec50c7abd4c597323
JaFe <kontakt.jakubfedoszczak@gmail.com>
2022-03-04 18:57:00 +0100

    Change styles of Info

--- src/components/atoms/Info/Info.ts
+++ src/components/atoms/Info/Info.ts
@@ -3,12 +3,11 @@ import styled from 'styled-components';
 const Info = styled.p`
   margin: 0;
   font-size: 1.6rem;
-  font-weight: bold;
+  font-weight: 500;
   color: ${({ theme }) => theme.colors.accentBlue};
   padding-inline: 2rem;
   text-align: center;
   opacity: 0.6;
-  letter-spacing: 1px;
 `;
 
 export default Info;
