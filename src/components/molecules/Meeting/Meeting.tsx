e7c3c196d6de7ddf991d8b387990dca632ab85f1
JaFe <kontakt.jakubfedoszczak@gmail.com>
2022-03-05 05:10:00 +0100

    Implement logic to UI

--- src/components/molecules/Meeting/Meeting.tsx
+++ src/components/molecules/Meeting/Meeting.tsx
@@ -18,38 +18,43 @@ import AcceptIcon from 'assets/icons/AcceptIcon.png';
 import CancelIcon from 'assets/icons/CancelIcon.png';
 import MailIcon from 'assets/icons/MailIcon.svg';
 import { authUser } from 'types/auth';
+import { useMeeting } from 'hooks/useMeeting';
 
 interface Props {
   meetHour: string;
-  user: authUser;
+  user: authUser & { meetingId: number };
 }
 
-const Meeting: React.FC<Props> = ({ meetHour, user }) => (
-  <>
-    <Time>
-      <Line />
-      <h1>{meetHour}</h1>
-      <SecondLine />
-    </Time>
-    <Wrapper>
-      <InfoWrapper>
-        <StudentBox icon={blueStudent} />
-        <StudentInfo>
-          <h1>
-            {user.first_name} {user.last_name}
-          </h1>
-          <EmailWrapper>
-            <p>{user.TextClassName}</p>
-            <StyledMail href={`mailto:${user.email}`} icon={MailIcon} />
-          </EmailWrapper>
-        </StudentInfo>
-      </InfoWrapper>
-      <BoxWrapper>
-        <EditBox icon={AcceptIcon} />
-        <DeleteBox icon={CancelIcon} />
-      </BoxWrapper>
-    </Wrapper>
-  </>
-);
+const Meeting: React.FC<Props> = ({ meetHour, user }) => {
+  const { deleteMeeting } = useMeeting();
+
+  return (
+    <>
+      <Time>
+        <Line />
+        <h1>{meetHour}</h1>
+        <SecondLine />
+      </Time>
+      <Wrapper>
+        <InfoWrapper>
+          <StudentBox icon={blueStudent} />
+          <StudentInfo>
+            <h1>
+              {user.first_name} {user.last_name}
+            </h1>
+            <EmailWrapper>
+              <p>{user.TextClassName}</p>
+              <StyledMail href={`mailto:${user.email}`} icon={MailIcon} />
+            </EmailWrapper>
+          </StudentInfo>
+        </InfoWrapper>
+        <BoxWrapper>
+          <EditBox icon={AcceptIcon} onClick={() => deleteMeeting(user.meetingId)} />
+          <DeleteBox icon={CancelIcon} onClick={() => deleteMeeting(user.meetingId)} />
+        </BoxWrapper>
+      </Wrapper>
+    </>
+  );
+};
 
 export default Meeting;
