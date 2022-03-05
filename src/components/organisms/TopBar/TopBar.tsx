34e44fd1bee0db530e178ba335ff38ed61563d13
TeodorWolski <teodorwolski88@gmail.com>
2022-03-05 09:45:12 +0100

    GoBack button finished

--- src/components/organisms/TopBar/TopBar.tsx
+++ src/components/organisms/TopBar/TopBar.tsx
@@ -1,11 +1,12 @@
 import { InnerWrapper, Logo, Wrapper, GoBack } from './TopBar.styles';
-import NotificationList from 'components/organisms/NotificationList/NotificationList';
 import SettingsIcon from 'assets/icons/SettingsIcon.png';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { storeRoot } from 'store';
 import { useAvatar } from 'hooks/useAvatar';
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import NotificationList from 'components/organisms/NotificationList/NotificationList';
 import BackIcon from 'assets/icons/UserSidebarIcons/BackIcon.svg';
 import SidebarLink from 'components/atoms/SidebarLink/SidebarLink';
 
@@ -15,6 +16,7 @@ const TopBar = () => {
   const user = useSelector((state: storeRoot) => state.user);
   const { getAvatarById } = useAvatar();
   const [image, setImage] = useState('');
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (user) {
@@ -29,7 +31,7 @@ const TopBar = () => {
     <Wrapper>
       <Logo>schoola</Logo>
       <InnerWrapper>
-        <GoBack icon={BackIcon} isComments={isComments} />
+        <GoBack icon={BackIcon} isComments={isComments} onClick={() => navigate(-1)} />
         <NotificationList />
         <SidebarLink as={Link} to="profile/settings" icon={SettingsIcon} />
       </InnerWrapper>
