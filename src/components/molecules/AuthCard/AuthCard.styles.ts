e225b71192930f0de1e451d4eb67c9e6b30f665e
JaFe <kontakt.jakubfedoszczak@gmail.com>
2022-03-06 07:53:14 +0100

    Correct AuthCard styles

--- src/components/molecules/AuthCard/AuthCard.styles.ts
+++ src/components/molecules/AuthCard/AuthCard.styles.ts
@@ -1,14 +1,15 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  width: 400px;
+  width: min(100%, 400px);
   min-height: 450px;
   padding-block: 40px;
   background-color: white;
   border-radius: 10px;
   box-shadow: 0 10px 20px -10px rgba(0, 0, 0, 0.2);
-  display: flex;
-  flex-direction: column;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 25% 65% 10%;
   align-items: center;
-  justify-content: center;
+  justify-items: center;
 `;
