c1e35db9b91c4872f849e1d3926d22c2ac95f194
TeodorWolski <teodorwolski88@gmail.com>
2022-03-17 20:33:05 +0100

    Trying to fix merge

--- src/components/atoms/ReloadWidget/ReloadWidget.tsx
+++ src/components/atoms/ReloadWidget/ReloadWidget.tsx
@@ -3,11 +3,7 @@ import { Wrapper } from './ReloadWidget.styles';
 import reloadSrc from 'assets/icons/ReloadIcon.png';
 
 const ReloadWidget = () => {
-  return (
-    <Wrapper onClick={() => window.location.reload()}>
-      <img src={reloadSrc} />
-    </Wrapper>
-  );
+  return <Wrapper onClick={() => window.location.reload()}>{/* <img src={reloadSrc} /> */}</Wrapper>;
 };
 
 export default ReloadWidget;
