7fbea5b6bb2f51436305030a39fff3d6a433e8a8
TeodorWolski <teodorwolski88@gmail.com>
2022-03-17 20:36:07 +0100

    Clearing and testing if i can merge

--- src/components/atoms/ReloadWidget/ReloadWidget.tsx
+++ src/components/atoms/ReloadWidget/ReloadWidget.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import { Wrapper } from './ReloadWidget.styles';
 import reloadSrc from 'assets/icons/ReloadIcon.png';
 
 const ReloadWidget = () => {
-  return <Wrapper onClick={() => window.location.reload()}>{/* <img src={reloadSrc} /> */}</Wrapper>;
+  return <Wrapper onClick={() => window.location.reload()}>{<img src={reloadSrc} alt="reload" />}</Wrapper>;
 };
 
 export default ReloadWidget;
