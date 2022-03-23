8ab4872e94ce35b59a3fc4f49595c2277e9f9ea6
TeodorWolski <teodorwolski88@gmail.com>
2022-03-23 21:48:01 +0100

    Adding the search link to the topbar

--- src/components/organisms/TopBar/TopBar.styles.ts
+++ src/components/organisms/TopBar/TopBar.styles.ts
@@ -24,6 +24,26 @@ export const Wrapper = styled.div`
   justify-content: space-around;
   padding: 1rem;
 `;
+
+export const StyledInput = styled.input<{ isVisible?: boolean }>`
+  position: absolute;
+  border: none;
+  padding: 1rem;
+  max-width: 18rem;
+  border-radius: 2rem;
+  top: 75%;
+  right: 30%;
+  display: ${({ isVisible }) => (isVisible ? 'flex' : 'none')};
+`;
+
+export const SettingsLink = styled(SidebarLink)<{ isSettings?: boolean }>`
+  display: ${({ isSettings }) => (isSettings ? 'block' : 'none')};
+`;
+
+export const SearchLink = styled(SidebarLink)<{ isSocialView?: boolean }>`
+  display: ${({ isSocialView }) => (isSocialView ? 'block' : 'none')};
+`;
+
 // export const UserPicture = styled.div`
 //   border-radius: 50%;
 //   width: 5rem;
@@ -44,7 +64,7 @@ export const InnerWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  margin-left: 10rem;
+  margin-left: 6rem;
 `;
 
 export const GoBack = styled(SidebarLink)<{ isComments?: boolean }>`
--- src/components/organisms/TopBar/TopBar.tsx
+++ src/components/organisms/TopBar/TopBar.tsx
@@ -1,13 +1,16 @@
-import { GoBack, InnerWrapper, Logo, Wrapper } from './TopBar.styles';
+import { useState } from 'react';
+import { GoBack, SearchLink, InnerWrapper, Logo, Wrapper, SettingsLink } from './TopBar.styles';
 import SettingsIcon from 'assets/icons/SettingsIcon.png';
 import { Link, useNavigate } from 'react-router-dom';
 import NotificationList from 'components/organisms/NotificationList/NotificationList';
 import BackIcon from 'assets/icons/UserSidebarIcons/BackIcon.svg';
-import SidebarLink from 'components/atoms/SidebarLink/SidebarLink';
+import SearchIcon from 'assets/icons/SearchIcon.svg';
 
 const TopBar = () => {
   const url = window.location.pathname;
   const isComments = url.includes('/comments');
+  const isSettings = url.includes('/profile');
+  const isSocialView = url.includes('/feed') || url.includes('spotted');
   const navigate = useNavigate();
 
   return (
@@ -17,8 +20,9 @@ const TopBar = () => {
       </Logo>
       <InnerWrapper>
         <GoBack icon={BackIcon} isComments={isComments} onClick={() => navigate(-1)} />
+        <SearchLink icon={SearchIcon} isSocialView={isSocialView} />
         <NotificationList />
-        <SidebarLink style={{ margin: 0 }} as={Link} to="/student/settings" icon={SettingsIcon} />
+        <SettingsLink isSettings={isSettings} style={{ margin: 0 }} as={Link} to="/student/settings" icon={SettingsIcon} />
       </InnerWrapper>
     </Wrapper>
   );
