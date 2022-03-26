ca7df8edcab38f085dc01cb8cbed1622f5743c20
TeodorWolski <teodorwolski88@gmail.com>
2022-03-26 13:21:48 +0100

    Adding the input to the search view

--- src/views/User/Search/Search.tsx
+++ src/views/User/Search/Search.tsx
@@ -1,8 +1,43 @@
+import styled from 'styled-components';
+
+export const PageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+const SearchInput = styled.input`
+  width: 90%;
+  padding: 1.2rem;
+  border: none;
+  border-radius: 2.5rem;
+  box-shadow: ${({ theme }) => theme.innerStyles.box};
+  outline: none;
+  margin-bottom: 1rem;
+`;
+
+const ResultsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 90%;
+`;
+
+const StudentResult = styled.div`
+  background-color: white;
+  height: 5rem;
+`;
+
 const Search = () => {
   return (
-    <div>
-      <h1>Search view</h1>
-    </div>
+    <PageWrapper>
+      <SearchInput placeholder="Wyszukaj ucznia, klasę etc." />
+      <ResultsWrapper>
+        <StudentResult>Student</StudentResult>
+        <StudentResult>Student</StudentResult>
+        <StudentResult>Student</StudentResult>
+      </ResultsWrapper>
+    </PageWrapper>
   );
 };
 
