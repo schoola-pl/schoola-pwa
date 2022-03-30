1f519de0fdf53c6f419264dab3bd16a2f4ccfd77
JaFe <kontakt.jakubfedoszczak@gmail.com>
2022-03-30 05:53:45 +0200

    Update ProtectedRoute provider

--- src/providers/ProtectedRoute.tsx
+++ src/providers/ProtectedRoute.tsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import { useAuth } from 'hooks/useAuth';
 import { loginRoute } from '../routes';
 import { useNavigate } from 'react-router';
-import { useAppLoading } from '../hooks/useAppLoading';
 
 interface props {
   Element: React.FC;
@@ -11,17 +10,12 @@ interface props {
 
 const ProtectedRoute: React.FC<props> = ({ Element, role }) => {
   const { currentUser, checkDoesRoleHasPermission } = useAuth();
-  const { setAppLoading } = useAppLoading();
   const navigate = useNavigate();
 
   useEffect(() => {
-    setAppLoading(true);
-    console.log('currentUser', currentUser);
     if (!currentUser || !checkDoesRoleHasPermission(role, currentUser.role)) {
-      setAppLoading(false);
       navigate(loginRoute);
     }
-    setAppLoading(false);
   }, [currentUser]);
 
   return <Element />;
