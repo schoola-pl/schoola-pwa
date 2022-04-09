48dc81f248c04a1b478cde2c3de4f3c53fa6168e
JaFe <kontakt.jakubfedoszczak@gmail.com>
2022-04-09 07:50:15 +0200

    Add auto isConfigured field changing in SchoolAdmin dashboard

--- src/components/templates/SchoolAdminTemplate/SchoolAdminTemplate.tsx
+++ src/components/templates/SchoolAdminTemplate/SchoolAdminTemplate.tsx
@@ -1,6 +1,6 @@
 import AdminSidebar from 'components/organisms/SchoolAdminSidebar/SchoolAdminSidebar';
 import { Wrapper } from './SchoolAdminTemplate.styles';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import ManageClasses from '../../../views/SchoolAdmin/ManageClasses/ManageClasses';
 import Dashboard from '../../../views/SchoolAdmin/Dashboard/Dashboard';
@@ -9,8 +9,24 @@ import ClassDetails from '../../../views/SchoolAdmin/ClassDetails/ClassDetails';
 import Error404 from '../../../views/Error404/Error404';
 import Settings from '../../../views/SchoolAdmin/Settings/Settings';
 import TooSmallScreen from 'components/organisms/TooSmallScreen/TooSmallScreen';
+import { useAuth } from '../../../hooks/useAuth';
 
 const SchoolAdminTemplate: React.FC = () => {
+  const { updateAttributes, currentUser } = useAuth();
+
+  // This use effect is just switching the user's attribute to 'yes'
+  useEffect(() => {
+    const isConfigured = currentUser?.attributes['custom:isConfigured'];
+    if (isConfigured) {
+      if (isConfigured === 'no')
+        updateAttributes({
+          attributes: {
+            'custom:isConfigured': 'yes'
+          }
+        });
+    }
+  }, [currentUser]);
+
   return window.innerWidth < 1360 ? (
     <TooSmallScreen />
   ) : (
