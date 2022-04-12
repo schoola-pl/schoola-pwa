a6e600ce9ddd11a254b288734866815855940005
JaFe <kontakt.jakubfedoszczak@gmail.com>
2022-04-12 05:22:18 +0200

    Add ReactGA to application

--- src/views/Root.tsx
+++ src/views/Root.tsx
@@ -1,5 +1,5 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useAppLoading } from 'hooks/useAppLoading';
 import AppLoader from '../components/molecules/AppLoader/AppLoader';
 import routes from '../routes';
@@ -9,10 +9,18 @@ import Modal from 'components/molecules/Modal/Modal';
 import { useModal } from 'hooks/useModal';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import ReactGA from 'react-ga';
+
+const TRACKING_ID = process.env.REACT_APP_TRACKING_ID;
+ReactGA.initialize(TRACKING_ID || '');
 
 const Root: React.FC = () => {
   const { isAppLoading, appLoadingText } = useAppLoading();
   const { isOpen, modalTitle, modalContent } = useModal();
+
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
 
   return (
     <>
