e52cc24b379c6353c4fcb0dbf6881b1c45361c35
TeodorWolski <teodorwolski88@gmail.com>
2022-04-16 11:52:14 +0200

    shit cleaned - styles exported

--- src/views/User/EditProfile/EditProfile.tsx
+++ src/views/User/EditProfile/EditProfile.tsx
@@ -1,112 +1,13 @@
-import styled from 'styled-components';
+import { PageWrapper, LinksWrapper, InfoWrapper, ProfilePictureWrapper, ImageWrapper, LinkWrapper, StyledIconDiv } from './EditProfile.styles';
 import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { storeRoot, useGetInterestedsQuery, useGetSocialsQuery } from 'store';
+import { storeRoot } from 'store';
 import { useAvatar } from 'hooks/useAvatar';
-import { useUser } from 'hooks/useUser';
 import { Link } from 'react-router-dom';
 import KeyIcon from 'assets/icons/KeyIcon.svg';
-import IconDiv from 'components/atoms/IconDiv/IconDiv';
 import NotificationIcon from 'assets/icons/NotificationIcon.svg';
 import LinkIcon from 'assets/icons/LinkIcon.svg';
 import PersonEditInterests from 'assets/icons/PersonEditInterests.svg';
-
-export const PageWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: center;
-
-  h1 {
-    font-weight: ${({ theme }) => theme.fontWeight.semibold};
-  }
-`;
-
-export const ProfilePictureWrapper = styled.div`
-  height: 10rem;
-  width: 10rem;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  cursor: pointer;
-  border-radius: 10rem;
-  background: linear-gradient(rgba(184, 208, 252, 1) 0%, rgba(91, 117, 166, 1) 0%, rgba(85, 171, 103, 1) 100%);
-
-  &:hover {
-    cursor: default;
-  }
-`;
-
-export const ImageWrapper = styled.div`
-  width: 9rem;
-  height: 9rem;
-  border-radius: inherit;
-  border: none;
-  overflow: hidden;
-
-  &:hover {
-    cursor: default;
-  }
-
-  img {
-    min-width: 100%;
-    background-color: white;
-    height: 100%;
-    object-fit: cover;
-  }
-`;
-
-const InfoWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-
-  input {
-    visibility: hidden;
-  }
-
-  label {
-    font-size: ${({ theme }) => theme.fontSize.xs};
-    color: ${({ theme }) => theme.colors.accentBlue};
-  }
-`;
-
-const LinksWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-`;
-
-const LinkWrapper = styled.div`
-  text-decoration: none;
-  color: black;
-  background-color: white;
-  font-size: 1.48rem;
-  height: 6rem;
-  width: 90%;
-  margin-bottom: 2rem;
-  border-radius: 1.5rem;
-  display: grid;
-  grid-template-columns: 20% 70% 10%;
-  align-items: center;
-  box-shadow: ${({ theme }) => theme.innerStyles.box};
-
-  &::after {
-    content: '>';
-  }
-`;
-
-const StyledIconDiv = styled(IconDiv)`
-  margin-left: 1.5rem;
-  background-color: ${({ theme }) => theme.colors.lightPurple};
-  height: 4.75rem;
-  width: 4.75rem;
-  border-radius: 1.5rem;
-  background-size: 70%;
-  position: relative;
-`;
 
 const EditProfile = () => {
   const user = useSelector((state: storeRoot) => state.user);
