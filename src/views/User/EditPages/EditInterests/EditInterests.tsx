a80b91ac12ef21ea186854cb821a99b98deef525
TeodorWolski <teodorwolski88@gmail.com>
2022-04-17 00:08:56 +0200

    Downloading interests

--- src/views/User/EditPages/EditInterests/EditInterests.tsx
+++ src/views/User/EditPages/EditInterests/EditInterests.tsx
@@ -1,7 +1,25 @@
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useUser } from 'hooks/useUser';
+import { storeRoot, useGetInterestedsQuery } from 'store';
+import styled from 'styled-components';
+import Interests from 'components/atoms/Interests/Interests';
+
+const Heading = styled.h1`
+  font-weight: ${({ theme }) => theme.fontWeight.semibold};
+`;
+
 const EditInterests = () => {
+  const user = useSelector((state: storeRoot) => state.user);
+  const { findInterested } = useUser();
+  const interesteds = useGetInterestedsQuery({});
+
   return (
     <div>
-      <h1>EditInterests</h1>
+      <Heading>Twoje zainteresowania</Heading>
+      {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
+      {/*@ts-ignore */}
+      <Interests interests={findInterested(user?.TextInteresteds.split(';'), interesteds?.data)} />
     </div>
   );
 };
