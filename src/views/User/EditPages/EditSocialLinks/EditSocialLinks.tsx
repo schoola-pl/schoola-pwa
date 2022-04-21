b7a5207f46e27355258cef607891f548d6214763
TeodorWolski <teodorwolski88@gmail.com>
2022-04-21 16:23:21 +0200

    Small changes

--- src/views/User/EditPages/EditSocialLinks/EditSocialLinks.tsx
+++ src/views/User/EditPages/EditSocialLinks/EditSocialLinks.tsx
@@ -1,6 +1,9 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 import { useSelector } from 'react-redux';
 import { storeRoot, useGetSocialsQuery } from 'store';
+import IconDiv from 'components/atoms/IconDiv/IconDiv';
+import BlueEditIcon from 'assets/icons/BlueEditIcon.svg';
 import FacebookIcon from 'assets/icons/SocialMediaIcons/FacebookIcon.svg';
 import InstagramIcon from 'assets/icons/SocialMediaIcons/InstagramIcon.svg';
 import BlogIcon from 'assets/icons/SocialMediaIcons/BlogIcon.svg';
@@ -9,8 +12,6 @@ import SpotifyIcon from 'assets/icons/SocialMediaIcons/SpotifyIcon.svg';
 import TikTokIcon from 'assets/icons/SocialMediaIcons/TikTokIcon.svg';
 import WebsiteIcon from 'assets/icons/SocialMediaIcons/WebsiteIcon.svg';
 import TwitterIcon from 'assets/icons/SocialMediaIcons/TwitterIcon.svg';
-
-const LinksList = styled.ul``;
 
 const getIconForPlatform = (platform: string) => {
   switch (platform) {
@@ -35,9 +36,35 @@ const getIconForPlatform = (platform: string) => {
   }
 };
 
+const Heading = styled.h1`
+  font-weight: ${({ theme }) => theme.fontWeight.medium};
+`;
+
 interface Props {
   icon: string;
 }
+
+const LinksList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+
+  li {
+    border-radius: 1rem;
+    box-shadow: ${({ theme }) => theme.innerStyles.box};
+    background-color: white;
+    width: 90%;
+    display: grid;
+    position: relative;
+    grid-template-columns: 80% 30%;
+    align-items: center;
+  }
+
+  h1 {
+    margin-left: 1rem;
+    font-weight: ${({ theme }) => theme.fontWeight.medium};
+  }
+`;
 
 export const SocialMediaLink = styled.a<Props>`
   height: 4rem;
@@ -57,33 +84,61 @@ export const SocialMediaLink = styled.a<Props>`
   }
 `;
 
+export const PageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+const IconsWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const StyledIconDiv = styled(IconDiv)`
+  padding: 1.8rem;
+  margin-right: 3rem;
+`;
+
 const EditSocialLinks = () => {
   const user = useSelector((state: storeRoot) => state.user);
   const socials = useGetSocialsQuery({
     userId: user?.TextSocials || null
   });
+  const [isInput, setToInput] = useState({ isEdit: false, id: 0 });
+  const handleChangeToInput = (id: number) => {
+    setToInput({ isEdit: true, id: id });
+  };
 
   return (
-    <div>
-      <h1>Edytuj linki społecznościowe</h1>
+    <PageWrapper>
+      <Heading>Edytuj linki społecznościowe</Heading>
       {socials.data && socials.data?.length > 0 ? (
-        <div>
+        <>
           {socials.data.map((social) => (
             <LinksList>
-              <SocialMediaLink
-                target="_blank"
-                rel="noopener noreferrer"
-                key={social.id}
-                href={social.url}
-                icon={getIconForPlatform(social.platform)}
-              />
+              <li>
+                {isInput.isEdit === true ? <input placeholder={`podaj link do ${social.platform}`} /> : <h1>{social.platform}</h1>}
+                <IconsWrapper>
+                  <StyledIconDiv as="button" onClick={() => handleChangeToInput(social.id)} icon={BlueEditIcon} />
+                  <SocialMediaLink
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    key={social.id}
+                    href={social.url}
+                    icon={getIconForPlatform(social.platform)}
+                  />
+                </IconsWrapper>
+              </li>
             </LinksList>
           ))}
-        </div>
+        </>
       ) : (
         <h1>brak linków społecznościowych - dodaj</h1>
       )}
-    </div>
+    </PageWrapper>
   );
 };
 export default EditSocialLinks;
