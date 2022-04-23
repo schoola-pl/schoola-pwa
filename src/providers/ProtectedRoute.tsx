050b598c7450dd794b691fe50b52bb799af21317
JaFe <kontakt.jakubfedoszczak@gmail.com>
2022-04-23 18:12:35 +0200

    Fix bug with notification connection

--- src/providers/ProtectedRoute.tsx
+++ src/providers/ProtectedRoute.tsx
@@ -18,16 +18,6 @@ const ProtectedRoute: React.FC<props> = ({ Element, role }) => {
 
   useEffect(() => {
     (async () => {
-      // Connect notification with user
-      const subscription = localStorage.getItem('notification_sub');
-      const isConnected = localStorage.getItem('notification_connected');
-      if (!isConnected && subscription) {
-        localStorage.setItem('notification_connected', 'true');
-        axios.post('https://notify.schoola.pl/api/v1/connect', {
-          subscription,
-          userId: user?.id
-        });
-      }
       // Checks is user logged in
       setAppLoading(true);
       if (await checkUser()) {
@@ -43,6 +33,16 @@ const ProtectedRoute: React.FC<props> = ({ Element, role }) => {
   useEffect(() => {
     // Checks is user object exists
     if (user && isAuthenticated) {
+      // Connect notification with user
+      const subscription = localStorage.getItem('notification_sub');
+      const isConnected = localStorage.getItem('notification_connected');
+      if (!isConnected && subscription) {
+        localStorage.setItem('notification_connected', 'true');
+        axios.post('https://notify.schoola.pl/api/v1/connect', {
+          subscription,
+          userId: user.id
+        });
+      }
       // Checks is user blocked
       if (!user.blocked) {
         // Checks if user has permission to access route
