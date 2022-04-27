b3fdac0b3a7a7323e179b8c4fa379a3e6e0db5e9
TeodorWolski <teodorwolski88@gmail.com>
2022-04-27 18:02:08 +0200

    Creating Select atom

--- src/views/User/EditPages/EditSocialLinks/EditSocialLinks.tsx
+++ src/views/User/EditPages/EditSocialLinks/EditSocialLinks.tsx
@@ -5,11 +5,34 @@ import { storeRoot, useGetSocialsQuery } from 'store';
 import SocialLinkCard from 'components/atoms/SocialLinkCard/SocialLinkCard';
 import { useModal } from 'hooks/useModal';
 import Button from 'components/atoms/Button/Button';
+import { options } from 'views/User/FirstLoginPages/LinksPage/options';
+import Select from 'components/atoms/Select/Select';
 
 const ModalWrapper = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
+
+  div {
+    display: flex;
+    align-items: center;
+  }
+
+  div > button {
+    margin-left: 0.5rem;
+  }
 `;
+
+const StyledForm = styled.form``;
+
+// const Select = styled.select`
+//   border-radius: 1rem;
+//   border: none;
+//   box-shadow: -2px 4px 10px rgba(115, 124, 142, 0.09);
+//   margin-right: 1rem;
+//   background-color: white;
+//   height: 4.25rem;
+// `;
 
 const EditSocialLinks = () => {
   const user = useSelector((state: storeRoot) => state.user);
@@ -26,9 +49,21 @@ const EditSocialLinks = () => {
           onClick={() =>
             openModal(
               <ModalWrapper>
-                <div>lolek</div> <Button onClick={closeModal}>zamknij</Button>
+                <StyledForm>
+                  <Select>
+                    {options.map((option) => (
+                      <option value={option.name}>{option.label}</option>
+                    ))}
+                  </Select>
+                </StyledForm>
+                <div>
+                  <Button>Dodaj</Button>
+                  <Button isRed onClick={closeModal}>
+                    Anuluj
+                  </Button>
+                </div>
               </ModalWrapper>,
-              'dodaj nowy link społ'
+              'Nowy link'
             )
           }
         >
